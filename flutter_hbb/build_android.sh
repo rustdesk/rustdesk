9ab5aec229f057927bb350c1aa64bf46d7a44088
rustdesk <info@rustdesk.com>
2021-08-22 22:34:09 +0800

    fix change id

--- flutter_hbb/build_android.sh
+++ flutter_hbb/build_android.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 flutter build apk --target-platform  android-arm64  --release --obfuscate --split-debug-info ./split-debug-info
+flutter build appbundle --target-platform  android-arm64  --release --obfuscate --split-debug-info ./split-debug-info
