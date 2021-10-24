972ec8d79d02ed68938b0a5825ae7f6fccd42bc5
cc-morning <90960002+cc-morning@users.noreply.github.com>
2021-10-24 23:47:02 +0800

    Fix missing clipboard content

--- src/server/clipboard_service.rs
+++ src/server/clipboard_service.rs
@@ -1,12 +1,11 @@
-use std::sync::mpsc::Sender;
-
-use clipboard_master::{CallbackResult, ClipboardHandler, Master};
-
 use super::*;
 pub use crate::common::{
     check_clipboard, ClipboardContext, CLIPBOARD_INTERVAL as INTERVAL, CLIPBOARD_NAME as NAME,
     CONTENT,
 };
+use clipboard_master::{CallbackResult, ClipboardHandler, Master};
+use hbb_common::ResultType;
+use std::{sync, sync::mpsc::Sender, thread};
 
 struct State {
     ctx: Option<ClipboardContext>,
@@ -31,41 +30,56 @@ impl super::service::Reset for State {
     }
 }
 
-struct ClipHandle {
-    tx: Sender<bool>,
-}
-
-impl ClipboardHandler for ClipHandle {
-    fn on_clipboard_change(&mut self) -> CallbackResult {
-        let _ = self.tx.send(true);
-        CallbackResult::Next
-    }
-}
-
 pub fn new() -> GenericService {
     let sp = GenericService::new(NAME, true);
-    sp.listen::<State, _, _>(notify, run);
+    sp.run::<_>(listen::run);
     sp
 }
 
-fn notify(tx: Sender<bool>) -> ResultType<()> {
-    Master::new(ClipHandle { tx }).run()?;
-    Ok(())
-}
+mod listen {
+    use super::*;
 
-fn run(sp: GenericService, state: &mut State) -> ResultType<()> {
-    if let Some(ctx) = state.ctx.as_mut() {
-        if let Some(msg) = check_clipboard(ctx, None) {
-            sp.send(msg);
-        }
-        sp.snapshot(|sps| {
-            let txt = crate::CONTENT.lock().unwrap().clone();
-            if !txt.is_empty() {
-                let msg_out = crate::create_clipboard_msg(txt);
-                sps.send_shared(Arc::new(msg_out));
-            }
-            Ok(())
-        })?;
+    struct ClipHandle {
+        tx: Sender<()>,
     }
-    Ok(())
+
+    impl ClipboardHandler for ClipHandle {
+        fn on_clipboard_change(&mut self) -> CallbackResult {
+            let _ = self.tx.send(());
+            CallbackResult::Next
+        }
+    }
+
+    fn notify(tx: Sender<()>) -> ResultType<()> {
+        Master::new(ClipHandle { tx }).run()?;
+        Ok(())
+    }
+
+    pub fn run(sp: GenericService) -> ResultType<()> {
+        let mut state = State::default();
+        let (tx, rx) = sync::mpsc::channel::<()>();
+        thread::spawn(|| {
+            let _ = notify(tx);
+        });
+
+        while sp.ok() {
+            sp.snapshot(|sps| {
+                let txt = crate::CONTENT.lock().unwrap().clone();
+                if !txt.is_empty() {
+                    let msg_out = crate::create_clipboard_msg(txt);
+                    sps.send_shared(Arc::new(msg_out));
+                }
+                Ok(())
+            })?;
+
+            if let Ok(()) = rx.recv() {
+                if let Some(ctx) = state.ctx.as_mut() {
+                    if let Some(msg) = check_clipboard(ctx, None) {
+                        sp.send(msg);
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
 }
