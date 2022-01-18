9f42fd534ccd2608324c51398200e5f7ce51b2ec
open-trade <zhouhuab@163.com>
2022-01-18 17:24:36 +0800

    put ogv.js to html

--- src/codec.js
+++ src/codec.js
@@ -1,11 +1,26 @@
-import { OGVLoader } from "ogv";
-
 // example: https://github.com/rgov/js-theora-decoder/blob/main/index.html
 // dev: copy decoder files from node/ogv/dist/* to project dir
 // dist: .... to dist
+/*
+	OGVDemuxerOggW: 'ogv-demuxer-ogg-wasm.js',
+	OGVDemuxerWebMW: 'ogv-demuxer-webm-wasm.js',
+	OGVDecoderAudioOpusW: 'ogv-decoder-audio-opus-wasm.js',
+	OGVDecoderAudioVorbisW: 'ogv-decoder-audio-vorbis-wasm.js',
+	OGVDecoderVideoTheoraW: 'ogv-decoder-video-theora-wasm.js',
+	OGVDecoderVideoVP8W: 'ogv-decoder-video-vp8-wasm.js',
+	OGVDecoderVideoVP8MTW: 'ogv-decoder-video-vp8-mt-wasm.js',
+	OGVDecoderVideoVP9W: 'ogv-decoder-video-vp9-wasm.js',
+	OGVDecoderVideoVP9SIMDW: 'ogv-decoder-video-vp9-simd-wasm.js',
+	OGVDecoderVideoVP9MTW: 'ogv-decoder-video-vp9-mt-wasm.js',
+	OGVDecoderVideoVP9SIMDMTW: 'ogv-decoder-video-vp9-simd-mt-wasm.js',
+	OGVDecoderVideoAV1W: 'ogv-decoder-video-av1-wasm.js',
+	OGVDecoderVideoAV1SIMDW: 'ogv-decoder-video-av1-simd-wasm.js',
+	OGVDecoderVideoAV1MTW: 'ogv-decoder-video-av1-mt-wasm.js',
+	OGVDecoderVideoAV1SIMDMTW: 'ogv-decoder-video-av1-simd-mt-wasm.js',
+*/
 
 export function loadVp9() {
-    OGVLoader.loadClass(
+    window.OGVLoader.loadClass(
         "OGVDecoderVideoVP9W",
         (videoCodecClass) => {
             videoCodecClass().then((decoder) => {
@@ -19,7 +34,7 @@ export function loadVp9() {
 }
 
 export function loadOpus() {
-    OGVLoader.loadClass(
+    window.OGVLoader.loadClass(
         "OGVDecoderAudioOpusW",
         (audioCodecClass) => {
             audioCodecClass().then((decoder) => {
