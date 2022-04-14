75c9a8c561828642fc2c8ecc4bd35c1ae9c0816c
rustdesk <info@rustdesk.com>
2022-04-14 20:44:43 +0800

    remote min size, android icon and scrap memory safety

--- libs/scrap/src/common/convert.rs
+++ libs/scrap/src/common/convert.rs
@@ -36,6 +36,19 @@ extern "C" {
         height: c_int,
     ) -> c_int;
 
+    pub fn ABGRToI420(
+        src_rgba: *const u8,
+        src_stride_rgba: c_int,
+        dst_y: *mut u8,
+        dst_stride_y: c_int,
+        dst_u: *mut u8,
+        dst_stride_u: c_int,
+        dst_v: *mut u8,
+        dst_stride_v: c_int,
+        width: c_int,
+        height: c_int,
+    ) -> c_int;
+
     pub fn NV12ToI420(
         src_y: *const u8,
         src_stride_y: c_int,
@@ -134,13 +147,35 @@ pub fn i420_to_rgb(width: usize, height: usize, src: &[u8], dst: &mut Vec<u8>) {
 pub fn bgra_to_i420(width: usize, height: usize, src: &[u8], dst: &mut Vec<u8>) {
     let (_, h, dst_stride_y, dst_stride_uv, u, v) =
         get_vpx_i420_stride(width, height, super::STRIDE_ALIGN);
-    let bps = 12;
-    dst.resize(h * dst_stride_y * bps / 8, 0);
+    dst.resize(h * dst_stride_y * 2, 0); // waste some memory to ensure memory safety
     let dst_y = dst.as_mut_ptr();
     let dst_u = dst[u..].as_mut_ptr();
     let dst_v = dst[v..].as_mut_ptr();
     unsafe {
         ARGBToI420(
+            src.as_ptr(),
+            (src.len() / height) as _,
+            dst_y,
+            dst_stride_y as _,
+            dst_u,
+            dst_stride_uv as _,
+            dst_v,
+            dst_stride_uv as _,
+            width as _,
+            height as _,
+        );
+    }
+}
+
+pub fn rgba_to_i420(width: usize, height: usize, src: &[u8], dst: &mut Vec<u8>) {
+    let (_, h, dst_stride_y, dst_stride_uv, u, v) =
+        get_vpx_i420_stride(width, height, super::STRIDE_ALIGN);
+    dst.resize(h * dst_stride_y * 2, 0); // waste some memory to ensure memory safety
+    let dst_y = dst.as_mut_ptr();
+    let dst_u = dst[u..].as_mut_ptr();
+    let dst_v = dst[v..].as_mut_ptr();
+    unsafe {
+        ABGRToI420(
             src.as_ptr(),
             (src.len() / height) as _,
             dst_y,
@@ -164,10 +199,9 @@ pub unsafe fn nv12_to_i420(
     height: usize,
     dst: &mut Vec<u8>,
 ) {
-    let (w, h, dst_stride_y, dst_stride_uv, u, v) =
+    let (_, h, dst_stride_y, dst_stride_uv, u, v) =
         get_vpx_i420_stride(width, height, super::STRIDE_ALIGN);
-    let bps = 12;
-    dst.resize(h * w * bps / 8, 0);
+    dst.resize(h * dst_stride_y * 2, 0); // waste some memory to ensure memory safety
     let dst_y = dst.as_mut_ptr();
     let dst_u = dst[u..].as_mut_ptr();
     let dst_v = dst[v..].as_mut_ptr();
