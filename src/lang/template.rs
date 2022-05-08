ed897f4b6d53cc44ad9cee0662ffe1e6db0c20ef
rustdesk <info@rustdesk.com>
2022-05-08 11:27:48 +0800

    update lang template

--- src/lang/template.rs
+++ src/lang/template.rs
@@ -265,5 +265,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("android_version_audio_tip", ""),
         ("android_start_service_tip", ""),
         ("Account", ""),
+        ("Quit", ""),
+        ("Help", ""),
     ].iter().cloned().collect();
 }
