7b3bbdf964bc974a6734b9da03fe7b36f2e2261b
Kingtous <kingtous@qq.com>
2022-05-29 19:55:50 +0800

    feat: add customed titlebar

---
+++ flutter/lib/desktop/widgets/titlebar_widget.dart
@@ -0,0 +1,66 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/material.dart';
+
+const sidebarColor = Color(0xFF0C6AF6);
+const backgroundStartColor = Color(0xFF7BBCF5);
+const backgroundEndColor = Color(0xFF0CCBF6);
+
+class DesktopTitleBar extends StatelessWidget {
+  final Widget? child;
+
+  const DesktopTitleBar({Key? key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [backgroundStartColor, backgroundEndColor],
+              stops: [0.0, 1.0]),
+        ),
+        child: WindowTitleBarBox(
+          child: Row(
+            children: [
+              Expanded(
+                  child: MoveWindow(
+                child: child,
+              )),
+              const WindowButtons()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+final buttonColors = WindowButtonColors(
+    iconNormal: const Color(0xFF805306),
+    mouseOver: const Color(0xFFF6A00C),
+    mouseDown: const Color(0xFF805306),
+    iconMouseOver: const Color(0xFF805306),
+    iconMouseDown: const Color(0xFFFFD500));
+
+final closeButtonColors = WindowButtonColors(
+    mouseOver: const Color(0xFFD32F2F),
+    mouseDown: const Color(0xFFB71C1C),
+    iconNormal: const Color(0xFF805306),
+    iconMouseOver: Colors.white);
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MinimizeWindowButton(colors: buttonColors),
+        MaximizeWindowButton(colors: buttonColors),
+        CloseWindowButton(colors: closeButtonColors),
+      ],
+    );
+  }
+}
--- flutter/macos/Runner/MainFlutterWindow.swift
+++ flutter/macos/Runner/MainFlutterWindow.swift
@@ -1,7 +1,8 @@
 import Cocoa
 import FlutterMacOS
+import bitsdojo_window_macos
 
-class MainFlutterWindow: NSWindow {
+class MainFlutterWindow: BitsdojoWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
@@ -11,5 +12,9 @@ class MainFlutterWindow: NSWindow {
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
+  }
+
+  override func bitsdojo_window_configure() -> UInt {
+    return BDW_CUSTOM_FRAME | BDW_HIDE_ON_STARTUP
   }
 }
