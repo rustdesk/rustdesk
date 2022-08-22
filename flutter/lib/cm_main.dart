f88bbb059556c1c8637ecae8defda125b5a3d843
csf <csf@breakbeat.cn>
2022-08-22 20:12:05 +0800

    update test cm_main.dart

--- flutter/lib/cm_main.dart
+++ flutter/lib/cm_main.dart
@@ -4,7 +4,9 @@ import 'package:flutter_hbb/main.dart';
 import 'package:get/get.dart';
 import 'package:window_manager/window_manager.dart';
 
+import 'common.dart';
 import 'desktop/pages/server_page.dart';
+import 'models/server_model.dart';
 
 /// -t lib/cm_main.dart to test cm
 void main(List<String> args) async {
@@ -13,5 +15,16 @@ void main(List<String> args) async {
   await windowManager.setSize(Size(400, 600));
   await windowManager.setAlignment(Alignment.topRight);
   await initEnv(kAppTypeConnectionManager);
-  runApp(GetMaterialApp(theme: getCurrentTheme(), home: DesktopServerPage()));
+  gFFI.serverModel.clients
+      .add(Client(0, false, false, "UserA", "123123123", true, false, false));
+  gFFI.serverModel.clients
+      .add(Client(1, false, false, "UserB", "221123123", true, false, false));
+  gFFI.serverModel.clients
+      .add(Client(2, false, false, "UserC", "331123123", true, false, false));
+  gFFI.serverModel.clients
+      .add(Client(3, false, false, "UserD", "441123123", true, false, false));
+  runApp(GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: getCurrentTheme(),
+      home: DesktopServerPage()));
 }
