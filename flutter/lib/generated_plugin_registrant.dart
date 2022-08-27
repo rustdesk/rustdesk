3b5b79712b241f997892a22e03a35ba2c7f29e98
Asura <todaymofish@gmail.com>
2022-08-27 22:17:02 +0800

    Fix compile error on macos

---
+++ flutter/lib/generated_plugin_registrant.dart
@@ -0,0 +1,35 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:desktop_drop/desktop_drop_web.dart';
+import 'package:device_info_plus_web/device_info_plus_web.dart';
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:package_info_plus_web/package_info_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+import 'package:wakelock_web/wakelock_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  DesktopDropWeb.registerWith(registrar);
+  DeviceInfoPlusPlugin.registerWith(registrar);
+  FirebaseAnalyticsWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  PackageInfoPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  WakelockWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
