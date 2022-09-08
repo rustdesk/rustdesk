e594657f979dcbd41c75f93b5d6e2664815c5fc5
21pages <pages21@163.com>
2022-09-08 12:20:33 +0800

    fix linux RUSTDESK_LIB_PATH
    Signed-off-by: 21pages <pages21@163.com>

--- flutter/linux/main.cc
+++ flutter/linux/main.cc
@@ -1,7 +1,7 @@
 #include <dlfcn.h>
 #include "my_application.h"
 
-#define RUSTDESK_LIB_PATH "ibrustdesk.so"
+#define RUSTDESK_LIB_PATH "librustdesk.so"
 // #define RUSTDESK_LIB_PATH "/usr/lib/rustdesk/librustdesk.so"
 typedef bool (*RustDeskCoreMain)();
 
