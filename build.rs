72e20e5020f4bad9bafa77c19d0dcba8f5cc0348
rustdesk <info@rustdesk.com>
2022-09-18 11:26:10 +0800

    move icon.ico to res

--- build.rs
+++ build.rs
@@ -11,12 +11,12 @@ fn build_manifest() {
     use std::io::Write;
     if std::env::var("PROFILE").unwrap() == "release" {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("icon.ico")
+        res.set_icon("res/icon.ico")
             .set_language(winapi::um::winnt::MAKELANGID(
                 winapi::um::winnt::LANG_ENGLISH,
                 winapi::um::winnt::SUBLANG_ENGLISH_US,
             ))
-            .set_manifest_file("manifest.xml");
+            .set_manifest_file("res/manifest.xml");
         match res.compile() {
             Err(e) => {
                 write!(std::io::stderr(), "{}", e).unwrap();
