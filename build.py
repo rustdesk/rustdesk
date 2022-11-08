6a50296fc345af624562a4eb454bd7d7abc41ccf
Kingtous <kingtous@qq.com>
2022-11-08 21:49:29 +0800

    opt: add more deps

--- build.py
+++ build.py
@@ -208,7 +208,7 @@ Version: %s
 Architecture: amd64
 Maintainer: open-trade <info@rustdesk.com>
 Homepage: https://rustdesk.com
-Depends: libgtk-3-0, libxcb-randr0, libxdo3, libxfixes3, libxcb-shape0, libxcb-xfixes0, libasound2, libsystemd0, pipewire, curl
+Depends: libgtk-3-0, libxcb-randr0, libxdo3, libxfixes3, libxcb-shape0, libxcb-xfixes0, libasound2, libsystemd0, pipewire, curl, libappindicator3-1, libva-drm2, libva-x11-2, libvdpau1
 Description: A remote control software.
 
 """ % version
