248f18f0d8092eea16a734320a3679ccbe3edade
21pages <pages21@163.com>
2022-11-27 16:56:02 +0800

    specify linux cm target size at beginning
    Signed-off-by: 21pages <pages21@163.com>

--- flutter/linux/main.cc
+++ flutter/linux/main.cc
@@ -4,6 +4,7 @@
 #define RUSTDESK_LIB_PATH "librustdesk.so"
 // #define RUSTDESK_LIB_PATH "/usr/lib/rustdesk/librustdesk.so"
 typedef bool (*RustDeskCoreMain)();
+bool gIsConnectionManager = false;
 
 bool flutter_rustdesk_core_main() {
    void* librustdesk = dlopen(RUSTDESK_LIB_PATH, RTLD_LAZY);
@@ -23,6 +24,11 @@ bool flutter_rustdesk_core_main() {
 int main(int argc, char** argv) {
   if (!flutter_rustdesk_core_main()) {
       return 0;
+  }
+  for (int i = 0; i < argc; i++) {
+    if (strcmp(argv[i], "--cm") == 0) {
+      gIsConnectionManager = true;
+    }
   }
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
