45c0e10102925c16cc6882b6c9542a396d50bf73
rustdesk <info@rustdesk.com>
2023-01-20 10:26:27 +0800

    applicationDidFinishLaunching

--- flutter/macos/Runner/AppDelegate.swift
+++ flutter/macos/Runner/AppDelegate.swift
@@ -3,13 +3,21 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+    var lauched = false;
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
       dummy_method_to_enforce_bundling()
     return true
   }
     
     override func applicationShouldOpenUntitledFile(_ sender: NSApplication) -> Bool {
-        handle_applicationShouldOpenUntitledFile();
+        if (lauched) {
+            handle_applicationShouldOpenUntitledFile();
+        }
         return true
+    }
+    
+    override func applicationDidFinishLaunching(_ aNotification: Notification) {
+        lauched = true;
+        NSApplication.shared.activate(ignoringOtherApps: true);
     }
 }
