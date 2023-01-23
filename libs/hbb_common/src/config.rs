d4851ebb4009e89f3de5fe81edd27409f158d26d
rustdesk <info@rustdesk.com>
2023-01-24 01:24:53 +0800

    revert 8fb3c452bea8dfb9c1de14db8b06f158d31147dd

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -49,10 +49,7 @@ lazy_static::lazy_static! {
     static ref CONFIG2: Arc<RwLock<Config2>> = Arc::new(RwLock::new(Config2::load()));
     static ref LOCAL_CONFIG: Arc<RwLock<LocalConfig>> = Arc::new(RwLock::new(LocalConfig::load()));
     pub static ref ONLINE: Arc<Mutex<HashMap<String, i64>>> = Default::default();
-    pub static ref PROD_RENDEZVOUS_SERVER: Arc<RwLock<String>> = Arc::new(RwLock::new(match option_env!("RENDEZVOUS_SERVER") {
-        Some(key) => key,
-        _ => "",
-    }.to_owned()));
+    pub static ref PROD_RENDEZVOUS_SERVER: Arc<RwLock<String>> = Default::default();
     pub static ref APP_NAME: Arc<RwLock<String>> = Arc::new(RwLock::new("RustDesk".to_owned()));
     static ref KEY_PAIR: Arc<Mutex<Option<(Vec<u8>, Vec<u8>)>>> = Default::default();
     static ref HW_CODEC_CONFIG: Arc<RwLock<HwCodecConfig>> = Arc::new(RwLock::new(HwCodecConfig::load()));
@@ -86,10 +83,7 @@ const RENDEZVOUS_SERVERS: &'static [&'static str] = &[
     "rs-cn.rustdesk.com",
 ];
 
-pub const RS_PUB_KEY: &'static str = match option_env!("RS_PUB_KEY") {
-    Some(key) => key,
-    None => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
-};
+pub const RS_PUB_KEY: &'static str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
