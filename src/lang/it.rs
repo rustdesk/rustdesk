a0caf8f257d43bc83df8edb6c17d5d2a3ec3ae1d
ilGigioVr88 <luigi.gemma@gmail.com>
2023-02-16 17:15:37 +0100

    Update it.rs

--- src/lang/it.rs
+++ src/lang/it.rs
@@ -450,6 +450,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Text chat", "Chat testuale"),
         ("Stop voice call", "Interrompi la chiamata vocale"),
         ("relay_hint_tip", ""),
-        ("Reconnect", ""),
+        ("Reconnect", "Riconnetti"),
         ].iter().cloned().collect();
 }
