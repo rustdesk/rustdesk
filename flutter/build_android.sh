1e1a544c9ec7ef93b2cd4a2041fcd245819b1357
enforcer007 <akhilgodvsdemon@gmail.com>
2023-02-21 07:00:59 +0000

    defaults to release

--- flutter/build_android.sh
+++ flutter/build_android.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MODE=${MODE:=debug}
+MODE=${MODE:=release}
 $ANDROID_NDK_HOME/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-strip android/app/src/main/jniLibs/arm64-v8a/*
 flutter build apk --target-platform android-arm64,android-arm --${MODE} --obfuscate --split-debug-info ./split-debug-info
 flutter build apk --split-per-abi --target-platform android-arm64,android-arm --${MODE} --obfuscate --split-debug-info ./split-debug-info
