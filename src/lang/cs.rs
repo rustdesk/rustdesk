513feca03f19916152249136de56f604ed963ad5
grummbeer <kde@streber24.de>
2023-03-07 22:03:52 +0100

    Translations. Add "RDP Settings"

--- src/lang/cs.rs
+++ src/lang/cs.rs
@@ -462,6 +462,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("No transfers in progress", ""),
         ("Set one-time password length", ""),
         ("idd_driver_tip", ""),
-        ("confirm_idd_driver_tip", "")
+        ("confirm_idd_driver_tip", ""),
+        ("RDP Settings", ""),
     ].iter().cloned().collect();
 }
