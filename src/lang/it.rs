3da68377ae9b0194bfa5a0377a01e4213f2b64c6
rustdesk <info@rustdesk.com>
2023-03-08 17:03:13 +0800

    fix it

--- src/lang/it.rs
+++ src/lang/it.rs
@@ -461,7 +461,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Resolution", "Risoluzione"),
         ("No transfers in progress", "Nessun trasferimento in corso"),
         ("Set one-time password length", "Imposta la lunghezza della password monouso"),
-        ("Set one-time password length", ""),
         ("idd_driver_tip", ""),
         ("confirm_idd_driver_tip", "")
     ].iter().cloned().collect();
