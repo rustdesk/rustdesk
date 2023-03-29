aace91ac5a96c8bdef2df1d5e0f14e28dc475950
asur4s <todaymofish@gmail.com>
2023-03-29 00:41:27 -0700

    Fix ci

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -36,6 +36,7 @@ pub fn is_xfce() -> bool {
 pub fn breakdown_callback() {
     #[cfg(target_os = "linux")]
     crate::input_service::clear_remapped_keycode();
+    #[cfg(not(any(target_os = "android", target_os = "ios")))]
     crate::input_service::release_modifiers();
 }
 
