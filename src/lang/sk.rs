b467b9d6573746d1a3adcee93621969550efeace
rustdesk <info@rustdesk.com>
2023-04-21 22:07:07 +0800

    fix #4143

--- src/lang/sk.rs
+++ src/lang/sk.rs
@@ -136,7 +136,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("ID does not exist", "ID neexistuje"),
         ("Failed to connect to rendezvous server", "Nepodarilo sa pripojiť k zoznamovaciemu serveru"),
         ("Please try later", "Vyskúšajte neskôr"),
-        ("Remote desktop is offline", "Vzdialená plocha je pripojená"),
+        ("Remote desktop is offline", "Vzdialená plocha nie je pripojená"),
         ("Key mismatch", "Kľúče sa nezhodujú"),
         ("Timeout", "Čas pre nadviazanie pripojenia vypršal"),
         ("Failed to connect to relay server", "Nepodarilo sa pripojiť k prepojovaciemu serveru"),
