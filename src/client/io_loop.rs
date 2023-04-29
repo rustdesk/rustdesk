ccb02d779ba909655dbb405c96e303f2fece4caa
21pages <pages21@163.com>
2023-04-29 10:13:27 +0800

    fix file transfer always connecting because of FingerPrintState not initialized
    Signed-off-by: 21pages <pages21@163.com>

--- src/client/io_loop.rs
+++ src/client/io_loop.rs
@@ -124,8 +124,10 @@ impl<T: InvokeUiSession> Remote<T> {
             Ok((mut peer, direct, pk)) => {
                 self.handler.set_connection_type(peer.is_secured(), direct); // flutter -> connection_ready
                 self.handler.set_connection_info(direct, false);
-                self.handler
-                    .set_fingerprint(crate::common::pk_to_fingerprint(pk.unwrap_or_default()));
+                if conn_type == ConnType::DEFAULT_CONN {
+                    self.handler
+                        .set_fingerprint(crate::common::pk_to_fingerprint(pk.unwrap_or_default()));
+                }
 
                 // just build for now
                 #[cfg(not(windows))]
