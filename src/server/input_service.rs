82396912708d0c3c9e5280bdc5a9361cae8d27a8
deep-soft <deep-soft@users.noreply.github.com>
2023-05-05 21:59:44 +0300

    Update input_service.rs

--- src/server/input_service.rs
+++ src/server/input_service.rs
@@ -675,7 +675,8 @@ fn fix_modifiers(modifiers: &[EnumOrUnknown<ControlKey>], en: &mut Enigo, ck: i3
 // See `run_pos` --> `set_cursor_position` --> `exclude`
 #[inline]
 pub fn update_latest_input_cursor_time(conn: i32) {
-    log_to_file(&format!("update_latest_input_cursor_time process id {}", std::process::id()));
+///1    log_to_file(&format!("update_latest_input_cursor_time process id {}", std::process::id()));
+    log::debug!(&format!("update_latest_input_cursor_time process id {}", std::process::id()));
     let mut lock = LATEST_PEER_INPUT_CURSOR.lock().unwrap();
     lock.conn = conn;
     lock.time = get_time();
