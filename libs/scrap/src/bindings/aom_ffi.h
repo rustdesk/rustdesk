a3f3bb4751cdc7db143e22376119d9346897cf88
21pages <pages21@163.com>
2023-05-08 19:57:32 +0800

    aom vcpkg binding
    Signed-off-by: 21pages <pages21@163.com>

---
+++ libs/scrap/src/bindings/aom_ffi.h
@@ -0,0 +1,8 @@
+#include <aom/aom.h>
+#include <aom/aom_image.h>
+#include <aom/aom_integer.h> 
+#include <aom/aom_codec.h>
+#include <aom/aom_external_partition.h>
+#include <aom/aom_frame_buffer.h> 
+#include <aom/aom_encoder.h>
+#include <aom/aomcx.h> 
