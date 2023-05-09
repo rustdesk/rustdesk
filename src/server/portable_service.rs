83cbf41efc8a9b5b5e4186bfce06f576258359a8
kathirvel <kathirvel.s@getryt.io>
2023-05-09 17:47:39 +0530

    portable service.rs file reverted

--- src/server/portable_service.rs
+++ src/server/portable_service.rs
@@ -566,9 +566,9 @@ pub mod client {
                             .home_dir()
                             .join("AppData")
                             .join("Local")
-                            .join("GetRytRemote-sciter");
+                            .join("RustDesk-sciter");
                         if std::fs::create_dir_all(&dir).is_ok() {
-                            let dst = dir.join("GetRytRemote.exe");
+                            let dst = dir.join("RustDesk.exe");
                             if std::fs::copy(&exe, &dst).is_ok() {
                                 if dst.exists() {
                                     if set_path_permission(&dir, "RX").is_ok() {
