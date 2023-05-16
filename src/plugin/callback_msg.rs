3d4abe046cf41d3d4b4f1b7dac448a726bf0c707
fufesou <shuanglongchen@yeah.net>
2023-05-16 20:29:58 +0800

    plugin_framework, remove unused fields for plugin signature verification
    Signed-off-by: fufesou <shuanglongchen@yeah.net>

--- src/plugin/callback_msg.rs
+++ src/plugin/callback_msg.rs
@@ -80,8 +80,6 @@ pub(super) struct MsgToExtSupport {
 
 #[derive(Debug, Serialize)]
 struct PluginSignReq {
-    uuid: Vec<u8>,
-    peer_id: String,
     plugin_id: String,
     version: String,
     msg: Vec<u8>,
@@ -285,8 +283,6 @@ fn request_plugin_sign(id: String, msg_to_rustdesk: MsgToRustDesk) -> PluginRetu
         let sign_url = format!("{}/lic/web/api/plugin-sign", get_api_server());
         let client = reqwest::blocking::Client::new();
         let req = PluginSignReq {
-            uuid: hbb_common::get_uuid(),
-            peer_id: crate::ui_interface::get_id(),
             plugin_id: id.clone(),
             version: signature_data.version,
             msg: signature_data.data,
