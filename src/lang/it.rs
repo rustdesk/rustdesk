67e3cef1956cedd912ec3bf80418dada8f4da799
bovirus <1262554+bovirus@users.noreply.github.com>
2023-06-20 15:01:08 +0200

    Update italian language

--- src/lang/it.rs
+++ src/lang/it.rs
@@ -221,7 +221,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Remember me", "Ricordami"),
         ("Trust this device", "Registra questo dispositivo come attendibile"),
         ("Verification code", "Codice di verifica"),
-        ("verification_tip", "È stato rilevato un nuovo dispositivo ed è stato inviato un codice di verifica all'indirizzo email registrato.\nPer continuare l'accesso inserisci il codice di verifica."),
+        ("verification_tip", "È stato inviato un codice di verifica all'indirizzo email registrato, per accedere inserisci il codice di verifica."),
         ("Logout", "Esci"),
         ("Tags", "Tag"),
         ("Search ID", "Cerca ID"),
