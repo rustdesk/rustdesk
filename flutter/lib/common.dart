4adaa4b83ad3b7eb8c31c5a285d406d85e3d2245
sysadmingithub <58550827+sysadmingithub@users.noreply.github.com>
2023-07-06 12:31:37 +0100

    Update common.dart no mytheme

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -165,16 +165,16 @@ class MyTheme {
   MyTheme._();
 
   static const Color grayBg = Color(0xFFEFEFF2);
-  static const Color accent = Color(0xFF0071FF);
-  static const Color accent50 = Color(0x770071FF);
-  static const Color accent80 = Color(0xAA0071FF);
+  static const Color accent = Color(0xFF05AD93);
+  static const Color accent50 = Color(0x7705AD93);
+  static const Color accent80 = Color(0xAA05AD93);
   static const Color canvasColor = Color(0xFF212121);
   static const Color border = Color(0xFFCCCCCC);
   static const Color idColor = Color(0xFF00B6F0);
-  static const Color darkGray = Color.fromARGB(255, 148, 148, 148);
+  static const Color darkGray = Color(0xFF018C77);
   static const Color cmIdColor = Color(0xFF21790B);
   static const Color dark = Colors.black87;
-  static const Color button = Color(0xFF2C8CFF);
+  static const Color button = Color(0xFF05AD93);
   static const Color hoverBorder = Color(0xFF999999);
 
   // ListTile
