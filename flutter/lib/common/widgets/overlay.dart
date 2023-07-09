89b3e68788f34d47931c6b0c09b8721e321e77a3
21pages <pages21@163.com>
2023-07-09 10:35:10 +0800

    add chatbox border
    Signed-off-by: 21pages <pages21@163.com>

--- flutter/lib/common/widgets/overlay.dart
+++ flutter/lib/common/widgets/overlay.dart
@@ -32,7 +32,7 @@ class DraggableChatWindow extends StatelessWidget {
         width: width,
         height: height,
         builder: (context, onPanUpdate) {
-          return isIOS
+          final child = isIOS
               ? ChatPage(chatModel: chatModel)
               : Scaffold(
                   resizeToAvoidBottomInset: false,
@@ -44,6 +44,10 @@ class DraggableChatWindow extends StatelessWidget {
                   ),
                   body: ChatPage(chatModel: chatModel),
                 );
+          return Container(
+              decoration:
+                  BoxDecoration(border: Border.all(color: MyTheme.border)),
+              child: child);
         });
   }
 
