93a641036088394740a1c0f2b4fff62835ce2ee8
FastAct <93490087+FastAct@users.noreply.github.com>
2023-07-24 07:11:55 +0200

    Update nl.rs

--- src/lang/nl.rs
+++ src/lang/nl.rs
@@ -517,6 +517,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Outgoing connection", "Uitgaande verbinding"),
         ("Exit", "Verlaten"),
         ("Open", "Open"),
-        ("logout_tip", ""),
+        ("logout_tip", "Weet je zeker dat je je wilt afmelden?"),
     ].iter().cloned().collect();
 }
