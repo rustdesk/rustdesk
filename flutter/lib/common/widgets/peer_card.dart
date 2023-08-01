e148cc7e51f805eb9979f13b4bd3436e23245c59
rustdesk <info@rustdesk.com>
2023-08-01 22:24:13 +0800

    fix issue #5224

--- flutter/lib/common/widgets/peer_card.dart
+++ flutter/lib/common/widgets/peer_card.dart
@@ -797,7 +797,6 @@ class RecentPeerCard extends BasePeerCard {
     if (peer.platform == 'Windows') {
       menuItems.add(_rdpAction(context, peer.id));
     }
-    menuItems.add(_wolAction(peer.id));
     if (Platform.isWindows) {
       menuItems.add(_createShortCutAction(peer.id));
     }
@@ -849,7 +848,6 @@ class FavoritePeerCard extends BasePeerCard {
     if (peer.platform == 'Windows') {
       menuItems.add(_rdpAction(context, peer.id));
     }
-    menuItems.add(_wolAction(peer.id));
     if (Platform.isWindows) {
       menuItems.add(_createShortCutAction(peer.id));
     }
@@ -950,7 +948,6 @@ class AddressBookPeerCard extends BasePeerCard {
     if (peer.platform == 'Windows') {
       menuItems.add(_rdpAction(context, peer.id));
     }
-    menuItems.add(_wolAction(peer.id));
     if (Platform.isWindows) {
       menuItems.add(_createShortCutAction(peer.id));
     }
@@ -1090,7 +1087,6 @@ class MyGroupPeerCard extends BasePeerCard {
     if (peer.platform == 'Windows') {
       menuItems.add(_rdpAction(context, peer.id));
     }
-    menuItems.add(_wolAction(peer.id));
     if (Platform.isWindows) {
       menuItems.add(_createShortCutAction(peer.id));
     }
