3baacac0eb342361e24e89ad9cf315c64f429847
roli8200 <roland.kaeser@intersoft-networks.ch>
2023-08-03 20:40:13 +0200

    Update config.rs
    asdf

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -90,12 +90,13 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com", "rs-sg.rustdesk.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.thurdata.ch"];
 
-pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
-    Some(key) if !key.is_empty() => key,
-    _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
-};
+//pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
+//    Some(key) if !key.is_empty() => key,
+//    _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
+//};
+pub const RS_PUB_KEY: &str = "yZrwYWuETijW4V3yk6BpsQ2AWrVuIUlx3KlRaQgBsUg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
