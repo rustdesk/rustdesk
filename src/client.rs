5028b8a93dfa2a08bf6d4a53b9a356a8544aaa2f
21pages <pages21@163.com>
2023-08-09 18:38:43 +0800

    fix keep peer card order when connect failed
    Signed-off-by: 21pages <pages21@163.com>

--- src/client.rs
+++ src/client.rs
@@ -1151,6 +1151,10 @@ impl LoginConfigHandler {
     ///
     /// * `config` - [`PeerConfig`] to save.
     pub fn save_config(&mut self, config: PeerConfig) {
+        if self.version == 0 {
+            log::info!("skip saving peer config {}", self.id);
+            return;
+        }
         config.store(&self.id);
         self.config = config;
     }
@@ -1209,10 +1213,6 @@ impl LoginConfigHandler {
     /// * `k` - key of option
     /// * `v` - value of option
     pub fn save_ui_flutter(&mut self, k: String, v: String) {
-        if self.version == 0 && k == "wm_" {
-            log::info!("skip saving {k}");
-            return;
-        }
         let mut config = self.load_config();
         config.ui_flutter.insert(k, v);
         self.save_config(config);
