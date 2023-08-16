542d86b667a2da19f89591a03a39edd9ab4d406e
rustdesk <info@rustdesk.com>
2023-08-16 18:57:56 +0800

    remove active_mouse_ which is very bad implementation

--- src/server/input_service.rs
+++ src/server/input_service.rs
@@ -707,7 +707,10 @@ fn get_last_input_cursor_pos() -> (i32, i32) {
     (lock.x, lock.y)
 }
 
+// check if mouse is moved by the controlled side user to make controlled side has higher mouse priority than remote.
 fn active_mouse_(conn: i32) -> bool {
+    true
+    /* this method is buggy (not working on macOS, making fast moving mouse event discarded here) and added latency (this is blocking way, must do in async way), so we disable it for now
     // out of time protection
     if LATEST_SYS_CURSOR_POS.lock().unwrap().0.elapsed() > MOUSE_MOVE_PROTECTION_TIMEOUT {
         return true;
@@ -760,6 +763,7 @@ fn active_mouse_(conn: i32) -> bool {
         }
         None => true,
     }
+    */
 }
 
 pub fn handle_pointer_(evt: &PointerDeviceEvent, conn: i32) {
