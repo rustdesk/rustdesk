c37732be9b2577cb6a9c28db332917963f5f3216
rustdesk <info@rustdesk.com>
2023-08-16 14:09:06 +0800

    correct translation

--- src/lang/tw.rs
+++ src/lang/tw.rs
@@ -537,6 +537,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Toggle Tags", ""),
         ("pull_ab_failed_tip", "未成功獲取地址簿"),
         ("push_ab_failed_tip", "未成功上傳地址簿"),
-        ("synced_peer_readded_tip", "最近會話中存在的設備將會被重新添加到地址簿。"),
+        ("synced_peer_readded_tip", "最近會話中存在的設備將會被重新同步到地址簿。"),
     ].iter().cloned().collect();
 }
