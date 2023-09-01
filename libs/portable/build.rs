446c1fd5724394818d43a98c577453a2d0711e9b
deep-soft <deep-soft@users.noreply.github.com>
2023-09-01 08:22:34 +0300

    Update build.rs

--- libs/portable/build.rs
+++ libs/portable/build.rs
@@ -1,5 +1,5 @@
 extern crate embed_resource;
 
 fn main() {
-    println!("cargo:rustc-link-lib=dylib:+verbatim=./flutter/build/windows/runner/rustdesk.dir/Release/Runner.res");
+    println!("cargo:rustc-link-lib=dylib:+verbatim=./Runner.res");
 }
