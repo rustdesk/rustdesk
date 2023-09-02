3fab42b8d1cb3a8290328b0a47d59db55f4cef7c
deep-soft <deep-soft@users.noreply.github.com>
2023-09-02 20:52:07 +0300

    Update build.rs

--- libs/portable/build.rs
+++ libs/portable/build.rs
@@ -2,9 +2,9 @@ extern crate embed_resource;
 use std::fs;
 
 fn main() {
-    let runner_res_path = "./Runner.res";
+    let runner_res_path = "Runner.res";
     match fs::metadata(runner_res_path) {
-        Ok(_) => println!("cargo:rustc-link-lib=dylib:+verbatim=./Runner.res"),
+        Ok(_) => println!("cargo:rustc-link-lib=dylib:+verbatim=./libs/portable/Runner.res"),
         Err(_) => embed_resource::compile("icon.rc", embed_resource::NONE),
     }
 }
