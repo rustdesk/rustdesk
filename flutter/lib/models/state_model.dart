b2404809fc98579ab19a5a49a5cfca3e6250338f
dignow <linlong1265@gmail.com>
2023-10-17 14:31:04 +0800

    trivial, add final
    Signed-off-by: dignow <linlong1265@gmail.com>

--- flutter/lib/models/state_model.dart
+++ flutter/lib/models/state_model.dart
@@ -11,7 +11,7 @@ enum SvcStatus { notReady, connecting, ready }
 class StateGlobal {
   int _windowId = -1;
   bool grabKeyboard = false;
-  RxBool _fullscreen = false.obs;
+  final RxBool _fullscreen = false.obs;
   bool _isMinimized = false;
   final RxBool isMaximized = false.obs;
   final RxBool _showTabBar = true.obs;
