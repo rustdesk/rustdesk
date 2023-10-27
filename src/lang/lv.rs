4dd19884e0620907dd4ad1f05e0773953ec3606e
Kleofass <4000163+Kleofass@users.noreply.github.com>
2023-10-27 16:06:32 +0300

    Update lv.rs

--- src/lang/lv.rs
+++ src/lang/lv.rs
@@ -566,9 +566,9 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Show displays as individual windows", "Rādīt displejus kā atsevišķus logus"),
         ("Use all my displays for the remote session", "Izmantot visus manus displejus attālajai sesijai"),
         ("selinux_tip", "Jūsu ierīcē ir iespējots SELinux, kas var neļaut RustDesk pareizi darboties kā kontrolētajai pusei."),
-        ("Change view", ""),
-        ("Big tiles", ""),
-        ("Small tiles", ""),
-        ("List", ""),
+        ("Change view", "Mainīt skatu"),
+        ("Big tiles", "Lielas flīzes"),
+        ("Small tiles", "Mazas flīzes"),
+        ("List", "Saraksts"),
     ].iter().cloned().collect();
 }
