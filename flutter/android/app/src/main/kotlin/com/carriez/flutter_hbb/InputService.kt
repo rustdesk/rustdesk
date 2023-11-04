6d8272472a9cb11f67350ca0cf3807ab313ca1a5
mcfans <mcfans@icloud.com>
2023-11-04 20:24:15 +0800

    fix: set correct flag

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/InputService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/InputService.kt
@@ -548,9 +548,10 @@ class InputService : AccessibilityService() {
         ctx = this
         val info = AccessibilityServiceInfo()
         if (Build.VERSION.SDK_INT >= 33) {
-            info.flags = FLAG_INPUT_METHOD_EDITOR
+            info.flags = FLAG_INPUT_METHOD_EDITOR or FLAG_RETRIEVE_INTERACTIVE_WINDOWS
+        } else {
+            info.flags = FLAG_RETRIEVE_INTERACTIVE_WINDOWS
         }
-        info.flags = FLAG_RETRIEVE_INTERACTIVE_WINDOWS
         setServiceInfo(info)
         fakeEditTextForTextStateCalculation = EditText(this)
         // Size here doesn't matter, we won't show this view.
