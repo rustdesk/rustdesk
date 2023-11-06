6ac2547dbe15822adb7d60d3c40c48313b1abe82
Sovince <100936649+Sovince37@users.noreply.github.com>
2023-11-06 19:18:16 +0100

    name

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -58,7 +58,7 @@ lazy_static::lazy_static! {
         _ => "",
     }.to_owned()));
     pub static ref EXE_RENDEZVOUS_SERVER: Arc<RwLock<String>> = Default::default();
-    pub static ref APP_NAME: Arc<RwLock<String>> = Arc::new(RwLock::new("AssistanceCCPH".to_owned()));
+    pub static ref APP_NAME: Arc<RwLock<String>> = Arc::new(RwLock::new("deskrust".to_owned()));
     static ref KEY_PAIR: Arc<Mutex<Option<KeyPair>>> = Default::default();
     static ref USER_DEFAULT_CONFIG: Arc<RwLock<(UserDefaultConfig, Instant)>> = Arc::new(RwLock::new((UserDefaultConfig::load(), Instant::now())));
     pub static ref NEW_STORED_PEER_CONFIG: Arc<Mutex<HashSet<String>>> = Default::default();
@@ -491,7 +491,7 @@ impl Config {
         let cfg = load_path(file);
         if suffix.is_empty() {
             log::trace!("{:?}", cfg);
-�   ??  }
+ÿ   Ǡݖ  }
         cfg
     }
 
