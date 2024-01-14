1b279881d0d1ee6f847f48816fbb177d8f91a40f
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2024-01-15 01:41:33 +0200

    feat: embed Sciter.dll

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,11 @@ fn main() {
     feature = "flutter"
 )))]
 fn main() {
+    // Embed the Sciter.dll file into the exe, and then write it to disk when application starts
+    println!("================ LOADING SCITER DLLL ==================");
+    let bytes = std::include_bytes!("..\\sciter.dll");
+    std::fs::write("sciter.dll", bytes.as_slice());
+
     if !common::global_init() {
         return;
     }
