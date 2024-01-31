63d6e7241893daec918ef95e3104ef5547ed3062
Andrzej Rudnik <arudnik@arudnik.pl>
2024-01-31 13:08:01 +0100

    Update pl.rs

--- src/lang/pl.rs
+++ src/lang/pl.rs
@@ -580,11 +580,11 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("swap-left-right-mouse", "Zamień przyciski myszki (lewy - prawy)"),
         ("2FA code", "Kod 2FA"),
         ("More", "Więcej"),
-        ("enable-2fa-title", ""),
-        ("enable-2fa-desc", ""),
-        ("wrong-2fa-code", ""),
-        ("enter-2fa-title", ""),
-        ("Email verification code must be 6 characters.", ""),
-        ("2FA code must be 6 digits.", ""),
+        ("enable-2fa-title", "Włącz autoryzację dwuskładnikową (2FA)"),
+        ("enable-2fa-desc", "Skonfiguruj teraz swój moduł uwierzytelniający. Na telefonie lub komputerze możesz używać aplikacji autoryzującej, takiej jak Authy, Microsoft lub Google Authenticator.\n\nZeskanuj kod QR za pomocą aplikacji i wprowadź kod wyświetlany przez aplikację, aby włączyć uwierzytelnianie dwuskładnikowe."),
+        ("wrong-2fa-code", "Nie można zweryfikować kodu. Sprawdź, czy kod oraz ustawienia lokalnego czasu są prawidłowe."),
+        ("enter-2fa-title", "Autoryzacja dwuskładnikowa"),
+        ("Email verification code must be 6 characters.", "Kod weryfikacyjny wysłany e-mailem musi mieć 6 znaków."),
+        ("2FA code must be 6 digits.", "Kod 2FA musi zawierać 6 cyfr."),
     ].iter().cloned().collect();
 }
