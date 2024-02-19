febe83c2c37558b507631853d0ae61cf05d463c7
fufesou <shuanglongchen@yeah.net>
2024-02-19 20:16:41 +0800

    Fix. Remove idd
    Signed-off-by: fufesou <shuanglongchen@yeah.net>

--- src/lang/template.rs
+++ src/lang/template.rs
@@ -458,8 +458,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Resolution", ""),
         ("No transfers in progress", ""),
         ("Set one-time password length", ""),
-        ("install_cert_tip", ""),
-        ("confirm_install_cert_tip", ""),
         ("RDP Settings", ""),
         ("Sort by", ""),
         ("New Connection", ""),
