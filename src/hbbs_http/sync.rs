b5a47efe8e4642b288714da3f4a4a838de71356c
rustdesk <info@rustdesk.com>
2024-03-07 16:33:13 +0800

    remove share rdp from strategy, useless and leading to confusion

--- src/hbbs_http/sync.rs
+++ src/hbbs_http/sync.rs
@@ -150,23 +150,11 @@ fn handle_config_options(config_options: HashMap<String, String>) {
     config_options
         .iter()
         .map(|(k, v)| {
-            if k == "allow-share-rdp" {
-                // only changes made after installation take effect.
-                #[cfg(windows)]
-                if crate::platform::is_installed() {
-                    let current = crate::ui_interface::is_share_rdp();
-                    let set = v == "Y";
-                    if current != set {
-                        crate::platform::windows::set_share_rdp(set);
-                    }
-                }
-            } else {
                 if v.is_empty() {
                     options.remove(k);
                 } else {
                     options.insert(k.to_string(), v.to_string());
                 }
-            }
         })
         .count();
     Config::set_options(options);
