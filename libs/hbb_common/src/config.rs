3d279ec127bbf81f172d07100efbd9f65735ca1d
fufesou <fufesou@users.noreply.github.com>
2024-03-15 17:46:36 +0800

    fix build (#7397)
    * fix build
    
    Signed-off-by: fufesou <shuanglongchen@yeah.net>
    
    * fmt
    
    Signed-off-by: fufesou <shuanglongchen@yeah.net>
    
    ---------
    
    Signed-off-by: fufesou <shuanglongchen@yeah.net>

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -25,7 +25,6 @@ use crate::{
         decrypt_str_or_original, decrypt_vec_or_original, encrypt_str_or_original,
         encrypt_vec_or_original, symmetric_crypt,
     },
-    platform::linux::run_cmds,
 };
 
 pub const RENDEZVOUS_TIMEOUT: u64 = 12_000;
@@ -403,10 +402,10 @@ fn patch(path: PathBuf) -> PathBuf {
         #[cfg(target_os = "linux")]
         {
             if _tmp == "/root" {
-                if let Ok(user) = run_cmds("whoami") {
+                if let Ok(user) = crate::platform::linux::run_cmds("whoami") {
                     if user != "root" {
                         let cmd = format!("getent passwd '{}' | awk -F':' '{{print $6}}'", user);
-                        if let Ok(output) = run_cmds(&cmd) {
+                        if let Ok(output) = crate::platform::linux::run_cmds(&cmd) {
                             return output.into();
                         }
                         return format!("/home/{user}").into();
