cde235896fb8b7be57d99a8005e997edcc85b12e
Mr-Update <37781396+Mr-Update@users.noreply.github.com>
2024-03-15 03:16:24 +0100

    Update de.rs (#7390)
    Co-authored-by: RustDesk <71636191+rustdesk@users.noreply.github.com>

--- src/lang/de.rs
+++ src/lang/de.rs
@@ -586,7 +586,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("2FA code must be 6 digits.", "Der 2FA-Code muss 6 Ziffern haben."),
         ("Multiple Windows sessions found", "Mehrere Windows-Sitzungen gefunden"),
         ("Please select the session you want to connect to", "Bitte wählen Sie die Sitzung, mit der Sie sich verbinden möchten"),
-        ("powered_by_me", ""),
-        ("outgoing_only_desk_tip", ""),
+        ("powered_by_me", "Unterstützt von RustDesk"),
+        ("outgoing_only_desk_tip", "Dies ist eine benutzerdefinierte Ausgabe.\nSie können eine Verbindung zu anderen Geräten herstellen, aber andere Geräte können keine Verbindung zu Ihrem Gerät herstellen."),
     ].iter().cloned().collect();
 }
