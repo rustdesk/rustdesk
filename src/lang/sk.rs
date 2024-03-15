8a705a6a9a9eb67ade8f239427cdf717c21b5fda
jxdv <virgoj@protonmail.com>
2024-03-15 02:14:03 +0000

    update sk.rs (#7387)
    Co-authored-by: RustDesk <71636191+rustdesk@users.noreply.github.com>

--- src/lang/sk.rs
+++ src/lang/sk.rs
@@ -586,7 +586,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("2FA code must be 6 digits.", "Kód 2FA musí obsahovať 6 číslic."),
         ("Multiple Windows sessions found", "Našlo sa viacero relácií systému Windows"),
         ("Please select the session you want to connect to", "Vyberte reláciu, ku ktorej sa chcete pripojiť"),
-        ("powered_by_me", ""),
+        ("powered_by_me", "Poháňané aplikáciou RustDesk"),
         ("outgoing_only_desk_tip", ""),
     ].iter().cloned().collect();
 }
