53bcd9d105ee088bc67ec4dedca4774b58a3a801
XLion <xlion@xlion.tw>
2024-03-24 20:29:44 +0800

    Update translation

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -587,7 +587,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Please select the session you want to connect to", "请选择您要连接的会话"),
         ("powered_by_me", "由 RustDesk 提供支持"),
         ("outgoing_only_desk_tip", "当前版本的软件是定制版本。\n您可以连接至其他设备，但是其他设备无法连接至您的设备。"),
-        ("preset_password_warning", ""),
+        ("preset_password_warning", "此定制版本附有预设密码。 任何知晓此密码的人都能完全控制您的设备。如果这不是您所预期的，请立即卸载此软件。"),
         ("Security Alert", ""),
         ("My address book", "我的地址簿"),
         ("Personal", "个人的"),
@@ -597,7 +597,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Read-only", "只读"),
         ("Read/Write", "读写"),
         ("Full Control", "完全控制"),
-        ("share_warning_tip", "上述字段是共享的并且对其他人可见。"),
+        ("share_warning_tip", "上述的字段為共享且对其他人可见。"),
         ("Everyone", "所有人"),
         ("ab_web_console_tip", "打开 Web 控制台以执行更多操作"),
     ].iter().cloned().collect();
