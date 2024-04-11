a5a8478ea5079ff1eabc0a2f5fd6768f747ecf52
felipebrambilla <108141878+felipebrambilla@users.noreply.github.com>
2024-04-11 14:48:12 -0300

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,8 +101,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["remote.infomaster.inf.br"];
+pub const PUBLIC_RS_PUB_KEY: &str = "Yo+zihdoCgbvcSDQ7kQSmAxuUKNXU3Awk34ZBq+xSi0=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
