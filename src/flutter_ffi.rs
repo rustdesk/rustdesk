56d353cb64fd215baa1e92cd14d3e8c64fb03e24
rustdesk <info@rustdesk.com>
2024-04-15 15:39:25 +0800

    fix me

--- src/flutter_ffi.rs
+++ src/flutter_ffi.rs
@@ -2108,7 +2108,7 @@ pub mod server_side {
     use crate::start_server;
 
     #[no_mangle]
-    pub unsafe extern "system" fn Java_ffi_startServer(
+    pub unsafe extern "system" fn Java_ffi_RustDesk_startServer(
         env: JNIEnv,
         _class: JClass,
         app_dir: JString,
@@ -2122,7 +2122,7 @@ pub mod server_side {
     }
 
     #[no_mangle]
-    pub unsafe extern "system" fn Java_ffi_startService(
+    pub unsafe extern "system" fn Java_ffi_RustDesk_startService(
         _env: JNIEnv,
         _class: JClass,
     ) {
@@ -2132,7 +2132,7 @@ pub mod server_side {
     }
 
     #[no_mangle]
-    pub unsafe extern "system" fn Java_ffi_translateLocale(
+    pub unsafe extern "system" fn Java_ffi_RustDesk_translateLocale(
         env: JNIEnv,
         _class: JClass,
         locale: JString,
@@ -2151,7 +2151,7 @@ pub mod server_side {
     }
 
     #[no_mangle]
-    pub unsafe extern "system" fn Java_ffi_refreshScreen(
+    pub unsafe extern "system" fn Java_ffi_RustDesk_refreshScreen(
         _env: JNIEnv,
         _class: JClass,
     ) {
