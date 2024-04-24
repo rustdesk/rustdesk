738450d3367e4a504446a4ea3c7135b925696336
yuluo <yuluozy@outlook.com>
2024-04-24 11:51:44 +0800

    test: test CI

--- flutter/ndk_arm.sh
+++ flutter/ndk_arm.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo armv7-linux-androideabi
 cargo ndk --platform 21 --target armv7-linux-androideabi build --release --features flutter,hwcodec
--- flutter/ndk_arm64.sh
+++ flutter/ndk_arm64.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo aarch64-linux-android
 cargo ndk --platform 21 --target aarch64-linux-android build --release --features flutter,hwcodec
