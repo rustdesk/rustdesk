2bf99630a04788a7a3fca29e25dd895f6a61cbba
bovirus <1262554+bovirus@users.noreply.github.com>
2024-05-08 21:58:57 +0200

    Update Italian language

--- src/lang/it.rs
+++ src/lang/it.rs
@@ -607,7 +607,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Follow remote window focus", "Segui focus finestra remota"),
         ("default_proxy_tip", "Protocollo e porta predefiniti sono Socks5 e 1080"),
         ("no_audio_input_device_tip", "Nessun dispositivo input audio trovato."),
-        ("Incoming", ""),
-        ("Outgoing", ""),
+        ("Incoming", "In entrata"),
+        ("Outgoing", "In uscita"),
     ].iter().cloned().collect();
 }
