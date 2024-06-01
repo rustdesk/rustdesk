8158813c2797fe482cb27bc29e50cb615e0f13e5
灰太狼大叔 <chen20030410@gmail.com>
2024-06-01 12:09:10 +0800

    更新 common.rs

--- src/common.rs
+++ src/common.rs
@@ -1052,12 +1052,20 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+<<<<<<< Updated upstream
+    "https://rustdesk.htlss.cn".to_owned()
+=======
+    "https://gz.htlss.cn:21114".to_owned()
+>>>>>>> Stashed changes
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
     let url = get_api_server(api, custom);
-    if url.is_empty() || url.contains("rustdesk.com") {
+<<<<<<< Updated upstream
+    if url.is_empty() || url.contains("rustdesk.htlss.cn") {
+=======
+    if url.is_empty() || url.contains("gz.htlss.cn") {
+>>>>>>> Stashed changes
         return "".to_owned();
     }
     format!("{}/api/audit/{}", url, typ)
