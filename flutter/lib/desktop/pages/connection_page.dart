50e996e554fd9a337f4174e4285300fa7d271e26
灰太狼大叔 <chen20030410@gmail.com>
2024-06-01 12:08:33 +0800

    更新 connection_page.dart
    修改部份

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -39,7 +39,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
   double? get height => bind.isIncomingOnly() ? null : em * 3;
 
   void onUsePublicServerGuide() {
-    const url = "https://rustdesk.com/pricing.html";
+    const url = "";
     canLaunchUrlString(url).then((can) {
       if (can) {
         launchUrlString(url);
