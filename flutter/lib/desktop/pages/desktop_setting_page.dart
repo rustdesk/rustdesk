5d7099b1f0899c76216c34e9e81e801294305c62
灰太狼大叔 <chen20030410@gmail.com>
2024-06-01 12:09:00 +0800

    更新 desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1727,7 +1727,7 @@ class _AboutState extends State<_About> {
                           .marginSymmetric(vertical: 4.0)),
                   InkWell(
                       onTap: () {
-                        launchUrlString('https://rustdesk.com/privacy.html');
+                        launchUrlString('https://rustdesk.com');
                       },
                       child: Text(
                         translate('Privacy Statement'),
@@ -1735,7 +1735,7 @@ class _AboutState extends State<_About> {
                       ).marginSymmetric(vertical: 4.0)),
                   InkWell(
                       onTap: () {
-                        launchUrlString('https://rustdesk.com');
+                        launchUrlString('https://pan.htlss.cn');
                       },
                       child: Text(
                         translate('Website'),
@@ -1753,7 +1753,7 @@ class _AboutState extends State<_About> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                                'Email:chen1350962574@Gmail.com | 1350962574@QQ.com',
                                 style: const TextStyle(color: Colors.white),
                               ),
                               Text(
