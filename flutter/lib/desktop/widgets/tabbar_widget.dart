d2a463f60ede4f3030174aabeead0501dd016152
fufesou <linlong1266@gmail.com>
2024-06-05 13:41:30 +0800

    Refact comments
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- flutter/lib/desktop/widgets/tabbar_widget.dart
+++ flutter/lib/desktop/widgets/tabbar_widget.dart
@@ -397,8 +397,8 @@ class DesktopTab extends StatelessWidget {
                     : null,
                 onPanStart: (_) => startDragging(isMainWindow),
                 onPanCancel: () {
-                  // We need to disable dragging because macOS handles dragging by default.
                   // We want to disable dragging of the tab area in the tab bar.
+                  // Disable dragging is needed because macOS handles dragging by default.
                   if (isMacOS) {
                     setMovable(isMainWindow, false);
                   }
