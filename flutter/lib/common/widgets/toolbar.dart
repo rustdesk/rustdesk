76fa5ccd747d3a66e7986edac30eab273618a2b5
fufesou <linlong1266@gmail.com>
2024-06-07 01:17:15 +0800

    refact: Wayland, do not show multi displays
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- flutter/lib/common/widgets/toolbar.dart
+++ flutter/lib/common/widgets/toolbar.dart
@@ -583,10 +583,11 @@ Future<List<TToggleMenu>> toolbarDisplayToggle(
         child: Text(translate('Lock after session end'))));
   }
 
-  if (bind.mainGetUseTextureRender() &&
+  if (!pi.isWayland &&
       pi.isSupportMultiDisplay &&
       PrivacyModeState.find(id).isEmpty &&
       pi.displaysCount.value > 1 &&
+      bind.mainGetUseTextureRender() &&
       bind.mainGetUserDefaultOption(key: kKeyShowMonitorsToolbar) == 'Y') {
     final value =
         bind.sessionGetDisplaysAsIndividualWindows(sessionId: ffi.sessionId) ==
