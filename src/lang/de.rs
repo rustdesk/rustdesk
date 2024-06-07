cd1081bb098f0cfa2c21423c45d962e324093406
Mr-Update <37781396+Mr-Update@users.noreply.github.com>
2024-06-08 00:31:20 +0200

    Update de.rs

--- src/lang/de.rs
+++ src/lang/de.rs
@@ -613,9 +613,9 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("clear_Wayland_screen_selection_tip", "Nachdem Sie die Bildschirmauswahl gelöscht haben, können Sie den freizugebenden Bildschirm erneut auswählen."),
         ("confirm_clear_Wayland_screen_selection_tip", "Sind Sie sicher, dass Sie die Auswahl des Wayland-Bildschirms löschen möchten?"),
         ("android_new_voice_call_tip", "Eine neue Sprachanrufanfrage wurde empfangen. Wenn Sie die Anfrage annehmen, wird der Ton auf Sprachkommunikation umgeschaltet."),
-        ("texture_render_tip", "Verwenden Sie Textur-Rendering, um die Bilder glatter zu machen."),
+        ("texture_render_tip", "Verwenden Sie Textur-Rendering, um die Bilder glatter zu machen. Sie können diese Option deaktivieren, wenn Sie Rendering-Probleme haben."),
         ("Use texture rendering", "Textur-Rendering verwenden"),
-        ("Floating window", ""),
-        ("floating_window_tip", ""),
+        ("Floating window", "Schwebendes Fenster"),
+        ("floating_window_tip", "Es hilft dabei, RustDesk im Hintergrund auszuführen."),
     ].iter().cloned().collect();
 }
