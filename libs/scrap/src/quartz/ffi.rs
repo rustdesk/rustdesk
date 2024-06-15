d62c40bb992ec02c5a2133f752d85da26a691648
21pages <sunboeasy@gmail.com>
2024-06-15 21:42:41 +0800

    mac scale factor of each screen
    Signed-off-by: 21pages <sunboeasy@gmail.com>

--- libs/scrap/src/quartz/ffi.rs
+++ libs/scrap/src/quartz/ffi.rs
@@ -193,7 +193,7 @@ extern "C" {
     pub fn CGDisplayIsOnline(display: u32) -> i32;
 
     pub fn CGDisplayBounds(display: u32) -> CGRect;
-    pub fn BackingScaleFactor() -> f32;
+    pub fn BackingScaleFactor(display: u32) -> f32;
 
     // IOSurface
 
