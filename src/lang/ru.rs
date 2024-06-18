ab228ef1d0de36208e8072e35d5995a4379d7e26
Stas Solovey <whats_up@tut.by>
2024-06-18 17:00:33 +0800

    Update ru.rs

--- src/lang/ru.rs
+++ src/lang/ru.rs
@@ -12,7 +12,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Start service", "Запустить службу"),
         ("Service is running", "Служба запущена"),
         ("Service is not running", "Служба не запущена"),
-        ("not_ready_status", "Не подключено. Проверьте соединение"),
+        ("not_ready_status", "Не подключено. Проверьте соединение."),
         ("Control Remote Desktop", "Управление удалённым рабочим столом"),
         ("Transfer file", "Передача файла"),
         ("Connect", "Подключиться"),
