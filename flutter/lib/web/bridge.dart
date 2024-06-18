818439db481f72adb5fcb6115ff2183910e95bda
rustdesk <info@rustdesk.com>
2024-06-18 08:43:19 +0800

    fix ci

--- flutter/lib/web/bridge.dart
+++ flutter/lib/web/bridge.dart
@@ -943,7 +943,7 @@ class RustdeskImpl {
     throw UnimplementedError();
   }
 
-  Future<String> mainVideoSaveDirectory({required bool root, dynamic hint}) {
+  String mainVideoSaveDirectory({required bool root, dynamic hint}) {
     throw UnimplementedError();
   }
 
