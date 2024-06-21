b76df086a4127383f913efd9aa52a40a4d454f67
fufesou <linlong1266@gmail.com>
2024-06-21 15:54:05 +0800

    Better voice call status control
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- flutter/lib/models/chat_model.dart
+++ flutter/lib/models/chat_model.dart
@@ -533,11 +533,12 @@ class ChatModel with ChangeNotifier {
   }
 
   void onVoiceCallClosed(String reason) {
-    if (_voiceCallStatus.value != VoiceCallStatus.notStarted) {
-      _voiceCallStatus.value = VoiceCallStatus.notStarted;
-      if (isAndroid) {
-        parent.target?.invokeMethod("on_voice_call_closed");
-      }
+    if (_voiceCallStatus.value == VoiceCallStatus.notStarted) {
+      return;
+    }
+    _voiceCallStatus.value = VoiceCallStatus.notStarted;
+    if (isAndroid) {
+      parent.target?.invokeMethod("on_voice_call_closed");
     }
   }
 
