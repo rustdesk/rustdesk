87addb8541b93a13eb6b91490261195715062824
21pages <pages21@163.com>
2024-07-09 14:45:40 +0800

    add custom client option to allow remote cm modification
    Signed-off-by: 21pages <sunboeasy@gmail.com>

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -2109,6 +2109,7 @@ pub mod keys {
     pub const OPTION_FLUTTER_PEER_TAB_VISIBLE: &str = "peer-tab-visible";
     pub const OPTION_FLUTTER_PEER_CARD_UI_TYLE: &str = "peer-card-ui-type";
     pub const OPTION_FLUTTER_CURRENT_AB_NAME: &str = "current-ab-name";
+    pub const OPTION_ALLOW_REMOTE_CM_MODIFICATION: &str = "allow-remote-cm-modification";
 
     // android floating window options
     pub const OPTION_DISABLE_FLOATING_WINDOW: &str = "disable-floating-window";
@@ -2195,6 +2196,7 @@ pub mod keys {
         "hide-server-settings",
         "hide-proxy-settings",
         "hide-username-on-card",
+        OPTION_ALLOW_REMOTE_CM_MODIFICATION,
     ];
     // DEFAULT_SETTINGS, OVERWRITE_SETTINGS
     pub const KEYS_SETTINGS: &[&str] = &[
