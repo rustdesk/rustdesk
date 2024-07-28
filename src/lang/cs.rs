19d1605d8c9b38d238b7ad4a18e32f7286027238
jxdv <virgoj@protonmail.com>
2024-07-28 09:13:15 +0000

    Update trs (#8866)
    * update sk-tr
    
    * update cs-tr

--- src/lang/cs.rs
+++ src/lang/cs.rs
@@ -629,8 +629,8 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("enable-bot-desc", "1, Otevřete chat s @BotFather.\n2, Pošlete příkaz \"/newbot\". Po dokončení tohoto kroku obdržíte token.\n3, Spusťte chat s nově vytvořeným botem. Pro jeho aktivaci odešlete zprávu začínající lomítkem vpřed (\"/\"), například \"/hello\".\n"),
         ("cancel-2fa-confirm-tip", "Jste si jisti, že chcete zrušit 2FA?"),
         ("cancel-bot-confirm-tip", "Jste si jisti, že chcete zrušit bota Telegramu?"),
-        ("About RustDesk", ""),
-        ("Send clipboard keystrokes", ""),
-        ("network_error_tip", ""),
+        ("About RustDesk", "O RustDesk"),
+        ("Send clipboard keystrokes", "Odesílat stisky kláves schránky"),
+        ("network_error_tip", "Zkontrolujte prosím připojení k síti a klikněte na tlačítko Opakovat."),
     ].iter().cloned().collect();
 }
