7ff257e597b555c71f57dc664df2f1490323db92
Shylock Hg <tcath2s@icloud.com>
2024-08-07 16:15:16 +0800

    Vcpkg set glib to linux only.

--- src/get_scale.rs
+++ src/get_scale.rs
@@ -1,11 +1,17 @@
+#[cfg(target_os = "linux")]
 use gdk4;
+#[cfg(target_os = "linux")]
 use gtk4;
+#[cfg(target_os = "linux")]
 use gdk4::prelude::{MonitorExt, DisplayExt, ListModelExt, Cast};
 
 fn main() {
-    let _ = gtk4::init();
-    let display = gdk4::Display::default().unwrap();
-    let monitor = display.monitors().item(0).unwrap().downcast::<gdk4::Monitor>().unwrap();
-    let scale = monitor.scale();
-    print!("{}", scale);
+    #[cfg(target_os = "linux")]
+    {
+        let _ = gtk4::init();
+        let display = gdk4::Display::default().unwrap();
+        let monitor = display.monitors().item(0).unwrap().downcast::<gdk4::Monitor>().unwrap();
+        let scale = monitor.scale();
+        print!("{}", scale);
+    }
 }
