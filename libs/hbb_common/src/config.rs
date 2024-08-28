6a342fc796bc32413ccbc06e188e03b5fbb5bb05
songques <songques@gmail.com>
2024-08-28 14:47:18 +0800

    private make
    1、配置私有IP
    2、屏蔽升级

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,14 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+// pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+// pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+
+// pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.songques.com"];
+// pub const PUBLIC_RS_PUB_KEY: &str = "TkQvNKhnvN6YVUxT2DpTEbPteLbKde+0TBPGQf1VjU0=";
+
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.94.136.90"];
+pub const PUBLIC_RS_PUB_KEY: &str = "TkQvNKhnvN6YVUxT2DpTEbPteLbKde+0TBPGQf1VjU0=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
