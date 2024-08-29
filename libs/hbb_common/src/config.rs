24288fe72997825982c3850a55c07b82c56f2c1a
热血格斗@神 <47322108+hxinbeng@users.noreply.github.com>
2024-08-29 16:25:29 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ylvpn.hxinbeng.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "g5i6WiZy7nUacFY70MV3UDcaKE0bXxZOLnCL5w3RFXA=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
