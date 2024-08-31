42ff8e9a97b47cbe6647f6652340fa0bb3df9980
cjx6662005 <95455069+cjx6662005@users.noreply.github.com>
2024-08-31 21:37:48 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://arcyn.f3322.net:7007".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
