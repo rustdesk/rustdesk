87e61139aab8801a1d3768250c4ef80224574ffc
wangjiya123 <1733647648@qq.com>
2024-09-07 00:20:06 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://ru-api.808888.best".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
