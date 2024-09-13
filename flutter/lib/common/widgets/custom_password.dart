9f9a22ec63c22e0d82418415c91a614f2f7c0235
rustdesk <info@rustdesk.com>
2024-09-13 08:46:21 +0800

    uppercase for all

--- flutter/lib/common/widgets/custom_password.dart
+++ flutter/lib/common/widgets/custom_password.dart
@@ -14,7 +14,11 @@ class UppercaseValidationRule extends ValidationRule {
   String get name => translate('uppercase');
   @override
   bool validate(String value) {
-    return value.contains(RegExp(r'[A-Z]'));
+    return value.runes.any((int rune) {
+      var character = String.fromCharCode(rune);
+      return character.toUpperCase() == character &&
+          character.toLowerCase() != character;
+    });
   }
 }
 
