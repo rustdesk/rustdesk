d3cffc9b3ab8bd1ccac500f63a69647beb59c0f7
slxar <41781584+slxar@users.noreply.github.com>
2024-09-13 23:48:34 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["foodoor.wshow.dev"];
+pub const PUBLIC_RS_PUB_KEY: &str = "SX8M24KV+CyFg7s7fPYZjbriG8S8KVQwfWK+G2FmC6E=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
