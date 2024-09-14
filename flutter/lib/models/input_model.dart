0a3affbad68097e91f74cf1dfe1ef05b6f089460
fufesou <linlong1266@gmail.com>
2024-09-14 22:06:14 +0800

    refact: web, keyboard, translate mode
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- flutter/lib/models/input_model.dart
+++ flutter/lib/models/input_model.dart
@@ -469,8 +469,12 @@ class InputModel {
 
   KeyEventResult handleRawKeyEvent(RawKeyEvent e) {
     if (isViewOnly) return KeyEventResult.handled;
-    if ((isDesktop || isWebDesktop) && !isInputSourceFlutter) {
-      return KeyEventResult.handled;
+    if (!isInputSourceFlutter) {
+      if (isDesktop) {
+        return KeyEventResult.handled;
+      } else if (isWeb) {
+        return KeyEventResult.ignored;
+      }
     }
 
     final key = e.logicalKey;
@@ -519,8 +523,12 @@ class InputModel {
 
   KeyEventResult handleKeyEvent(KeyEvent e) {
     if (isViewOnly) return KeyEventResult.handled;
-    if ((isDesktop || isWebDesktop) && !isInputSourceFlutter) {
-      return KeyEventResult.handled;
+    if (!isInputSourceFlutter) {
+      if (isDesktop) {
+        return KeyEventResult.handled;
+      } else if (isWeb) {
+        return KeyEventResult.ignored;
+      }
     }
     if (isWindows || isLinux) {
       // Ignore meta keys. Because flutter window will loose focus if meta key is pressed.
