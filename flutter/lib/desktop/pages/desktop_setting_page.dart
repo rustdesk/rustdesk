f99d8dbe58120905a39ceaa0664950d292f54027
Dr. Stefan Brinkhoff <brinkhoff@locandis.de>
2024-09-24 13:59:38 +0200

    remove slogan

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1934,15 +1934,9 @@ class _AboutState extends State<_About> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                'Copyright © ${DateTime.now().toString().substring(0, 4)} ondental.\n$license',
+                                'Copyright © ${DateTime.now().toString().substring(0, 4)} Brinkhoff Consulting GmbH.\n$license',
                                 style: const TextStyle(color: Colors.white),
                               ),
-                              Text(
-                                translate('Slogan_tip'),
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w800,
-                                    color: Colors.white),
-                              )
                             ],
                           ),
                         ),
