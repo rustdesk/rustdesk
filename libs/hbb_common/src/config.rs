a81d8ce9716b047b2c2a20cb79b5f51b9a0142c5
qdzmc <32753790+qdzmc@users.noreply.github.com>
2024-10-10 09:36:31 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["jm.wuxue.cool"];
-pub const PUBLIC_RS_PUB_KEY: &str = "qIjWf3TpZHrosC93tQWDN4G7+PHdeoI7azpSFiOLgEM=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["cy.wuxue.cool"];
+pub const PUBLIC_RS_PUB_KEY: &str = "OVXBAcI1ACa+Khs9ZNNDJ3fauPq2VxlyqldxYmT1wSI=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
