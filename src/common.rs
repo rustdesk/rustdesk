0cd6a682d37b1ea395f0338fdfca190fbe20bcb3
drny888 <155070976+drny888@users.noreply.github.com>
2024-10-11 18:42:09 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rd.skck168.com:7000".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
