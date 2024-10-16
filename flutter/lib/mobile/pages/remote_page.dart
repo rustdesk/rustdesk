5e920f0fd048e7e49046a17f833657a26d4c001a
rustdesk <info@rustdesk.com>
2024-10-16 22:37:03 +0800

    fix ci

--- flutter/lib/mobile/pages/remote_page.dart
+++ flutter/lib/mobile/pages/remote_page.dart
@@ -242,16 +242,16 @@ class _RemotePageState extends State<RemotePage> {
     }
   }
 
-  async void handleSoftKeyboardInput(String newValue) {
+  Future<void> handleSoftKeyboardInput(String newValue) async {
     if (isIOS) {
       // fix: TextFormField onChanged event triggered multiple times when Korean input
       // https://github.com/rustdesk/rustdesk/pull/9644
       await Future.delayed(const Duration(milliseconds: 10));
 
       if (newValue != _textController.text) return;
-      return _handleIOSSoftKeyboardInput(_textController.text);
+      _handleIOSSoftKeyboardInput(_textController.text);
     } else {
-      return _handleNonIOSSoftKeyboardInput(newValue);
+      _handleNonIOSSoftKeyboardInput(newValue);
     }
   }
 
