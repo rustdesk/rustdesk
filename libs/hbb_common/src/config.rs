da40220100ca2e777894b10b84fdeb719aa5e40d
ztdesk1 <167518742+ztdesk1@users.noreply.github.com>
2024-10-19 23:59:15 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -72,7 +72,7 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
      pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), "123456AA".to_string());
+        map.insert("password".to_string(), "123456".to_string());
         RwLock::new(map)
     };
  pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
