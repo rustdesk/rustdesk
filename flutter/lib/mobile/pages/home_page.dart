f2a6aa7042cf3e05a097662837feaa8e5319e953
haisong8822 <haisongnb@qq.com>
2024-10-30 00:10:31 +0800

    home_page.dart

--- flutter/lib/mobile/pages/home_page.dart
+++ flutter/lib/mobile/pages/home_page.dart
@@ -77,7 +77,7 @@ class HomePageState extends State<HomePage> {
           // backgroundColor: MyTheme.grayBg,
           appBar: AppBar(
             centerTitle: true,
-            title: appTitle(),
+            title: Text("赢商动力"),
             actions: _pages.elementAt(_selectedIndex).appBarActions,
           ),
           bottomNavigationBar: BottomNavigationBar(
