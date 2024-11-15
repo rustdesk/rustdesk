55dc93257efb8d61a48f5a9c35affc8b3adf3c20
kkylsb <65747935+kkylsb@users.noreply.github.com>
2024-11-15 16:03:05 +0800

    VolumeController.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/VolumeController.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/VolumeController.kt
@@ -1,4 +1,4 @@
-package com.carriez.flutter_hbb
+package com.kk.yl
 
 // Inspired by https://github.com/yosemiteyss/flutter_volume_controller/blob/main/android/src/main/kotlin/com/yosemiteyss/flutter_volume_controller/VolumeController.kt
 
