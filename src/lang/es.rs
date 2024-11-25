bf9ad876f50e55320d294ea9f46ba8fa624c6ac9
flusheDData <116861809+flusheDData@users.noreply.github.com>
2024-11-25 16:44:48 +0100

    Update es.rs
    New term added

--- src/lang/es.rs
+++ src/lang/es.rs
@@ -653,6 +653,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Upload folder", "Subir carpeta"),
         ("Upload files", "Subir archivos"),
         ("Clipboard is synchronized", "Portapapeles sincronizado"),
-        ("Update client clipboard", ""),
+        ("Update client clipboard", "Actualizar portapapeles del cliente"),
     ].iter().cloned().collect();
 }
