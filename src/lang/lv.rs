f8c2713c5bc945f0758026c33558515d5e7a4719
Kleofass <4000163+Kleofass@users.noreply.github.com>
2024-11-30 18:27:39 +0200

    Update lv.rs (#10124)

--- src/lang/lv.rs
+++ src/lang/lv.rs
@@ -653,7 +653,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Upload folder", "Augšupielādēt mapi"),
         ("Upload files", "Augšupielādēt failus"),
         ("Clipboard is synchronized", "Starpliktuve ir sinhronizēta"),
-        ("Update client clipboard", ""),
-        ("Untagged", ""),
+        ("Update client clipboard", "Atjaunināt klienta starpliktuvi"),
+        ("Untagged", "Neatzīmēts"),
     ].iter().cloned().collect();
 }
