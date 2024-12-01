97e6166a2d4dcc74cc5fcfee4f19cab8dee7f6c0
RustDesk <71636191+rustdesk@users.noreply.github.com>
2024-12-01 17:17:56 +0800

    Update my_application.cc

--- flutter/linux/my_application.cc
+++ flutter/linux/my_application.cc
@@ -44,7 +44,7 @@ static void my_application_activate(GApplication* application) {
   GdkScreen* screen = NULL;
 #ifdef GDK_WINDOWING_X11
   screen = gtk_window_get_screen(window);
-  if (GDK_IS_X11_SCREEN(screen)) {
+  if (screen != NULL && GDK_IS_X11_SCREEN(screen)) {
     const gchar* wm_name = gdk_x11_screen_get_window_manager_name(screen);
     if (g_strcmp0(wm_name, "GNOME Shell") != 0) {
       use_header_bar = FALSE;
