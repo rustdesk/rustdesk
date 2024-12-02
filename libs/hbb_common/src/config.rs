f07b067713e37a5520ce71d5bc3610c432ab580b
gmask <65796692+gmask@users.noreply.github.com>
2024-12-02 13:05:56 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["5n66a01879.oicp.vip"];
+pub const PUBLIC_RS_PUB_KEY: &str = "nEO8Co97NVHgRLLZYJq8LuKUYaQzQ6Q4tqCOH4MjxBQ=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
