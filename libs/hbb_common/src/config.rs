a5e151271905dbb225753358e6e5ebbb54c5f66d
AsiaKing1103 <132553794+AsiaKing1103@users.noreply.github.com>
2024-12-03 13:44:37 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["https://All-FastData.myasustor.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["All-FastData.myasustor.com"];
 pub const PUBLIC_RS_PUB_KEY: &str = "NzWSDq9ZG870EqwhSgWkATjaEstICOqXfSr2F5yJ8Y8=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
