581ffce9dfacbfb968d194968a3c47bc03fd2950
d2-fnb <dota2fanboyytb@gmail.com>
2024-12-10 02:41:35 +0530

    feat: enable customer mode by default

--- src/core_main.rs
+++ src/core_main.rs
@@ -30,6 +30,10 @@ macro_rules! my_println{
 #[cfg(not(any(target_os = "android", target_os = "ios")))]
 pub fn core_main() -> Option<Vec<String>> {
     crate::load_custom_client();
+    
+    // only for customer
+    crate::enable_customer_mode();
+    
     #[cfg(windows)]
     crate::platform::windows::bootstrap();
     let mut args = Vec::new();
