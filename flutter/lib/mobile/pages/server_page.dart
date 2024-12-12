8eda6180f4deb5c9aa175c9d28e42c6472089aa1
Salem-Su <113160509+Salem-Su@users.noreply.github.com>
2024-12-13 01:20:08 +0800

    Update server_page.dart

--- flutter/lib/mobile/pages/server_page.dart
+++ flutter/lib/mobile/pages/server_page.dart
@@ -232,13 +232,13 @@ class ServiceNotRunningNotification extends StatelessWidget {
             ElevatedButton.icon(
                 icon: const Icon(Icons.play_arrow),
                 onPressed: () {
-                  if (gFFI.userModel.userName.value.isEmpty &&
-                      bind.mainGetLocalOption(key: "show-scam-warning") !=
-                          "N") {
-                    showScamWarning(context, serverModel);
-                  } else {
+                  //if (gFFI.userModel.userName.value.isEmpty &&
+                  //    bind.mainGetLocalOption(key: "show-scam-warning") !=
+                  //        "N") {
+                  //  showScamWarning(context, serverModel);
+                  //} else {
                     serverModel.toggleService();
-                  }
+                  //}
                 },
                 label: Text(translate("Start service")))
           ],
