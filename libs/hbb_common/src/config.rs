deb6bd8f2c58d3e4493d979ae13b6c8edb73f34c
MA1174 <48651578+MA1174@users.noreply.github.com>
2024-12-23 14:18:09 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["ik.zy10086.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "YDtwWBis497egO6zv14PeXFlEP7qac04lg5d+MVXZcA=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.20021002.xyz"];
+pub const PUBLIC_RS_PUB_KEY: &str = "J6gPgPi6MXZeiGUxW0gxO4caATm8NpTDeW+ouWttmnc=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
