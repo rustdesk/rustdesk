96ec51c60a8b5e34cf285cfd1f7314f0e50db327
热血格斗@神 <47322108+hxinbeng@users.noreply.github.com>
2024-12-24 14:56:35 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rs-api.hxinbeng.top:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
