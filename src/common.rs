cc8189fa50dd8c6581cf4a759333ea6fe04e17de
dearneo <chenhao.neo@gmail.com>
2024-12-24 16:50:40 +0700

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://rustdesk.th.kex-express.com:21114".to_owned()
+    "http://kexrd.th.kex-express.com:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
