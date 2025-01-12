cabf4c2a1583b7e1e7d80313e3563b0bcf62c3d4
yuntaihk <yuntaihk@126.com>
2025-01-12 10:32:42 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -816,7 +816,7 @@ pub fn check_software_update() {
 
 #[tokio::main(flavor = "current_thread")]
 async fn check_software_update_() -> hbb_common::ResultType<()> {
-    let url = "https://github.com/rustdesk/rustdesk/releases/latest";
+    let url = "https://github.com/yuntaihk/rustdesk/releases/latest";
     let latest_release_response = create_http_client_async().get(url).send().await?;
     let latest_release_version = latest_release_response
         .url()
@@ -909,12 +909,12 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://my.snowsurfer.cn:7000".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
     let url = get_api_server(api, custom);
-    if url.is_empty() || url.contains("rustdesk.com") {
+    if url.is_empty() || url.contains("SnowSurfer.cn") {
         return "".to_owned();
     }
     format!("{}/api/audit/{}", url, typ)
