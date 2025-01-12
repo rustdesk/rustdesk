60421d9f2ba7469c943c9e1bb609e261f791c4a6
yuntaihk <yuntaihk@126.com>
2025-01-12 10:22:33 +0800

    settings_page.dart

--- flutter/lib/mobile/pages/settings_page.dart
+++ flutter/lib/mobile/pages/settings_page.dart
@@ -36,7 +36,7 @@ class SettingsPage extends StatefulWidget implements PageShape {
   State<SettingsPage> createState() => _SettingsState();
 }
 
-const url = 'https://rustdesk.com/';
+const url = 'http://Down.SnowSurfer.cn/';
 
 enum KeepScreenOn {
   never,
@@ -787,7 +787,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
                 title: Text(translate("Version: ") + version),
                 value: Padding(
                   padding: EdgeInsets.symmetric(vertical: 8),
-                  child: Text('rustdesk.com',
+                  child: Text('SnowSurfer.cn',
                       style: TextStyle(
                         decoration: TextDecoration.underline,
                       )),
@@ -812,7 +812,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
             SettingsTile(
               title: Text(translate("Privacy Statement")),
               onPressed: (context) =>
-                  launchUrlString('https://rustdesk.com/privacy.html'),
+                  launchUrlString('http://Down.SnowSurfer.cn/privacy.html'),
               leading: Icon(Icons.privacy_tip),
             )
           ],
@@ -925,12 +925,12 @@ void showAbout(OverlayDialogManager dialogManager) {
         Text('Version: $version'),
         InkWell(
             onTap: () async {
-              const url = 'https://rustdesk.com/';
+              const url = 'http://Down.SnowSurfer.cn/';
               await launchUrl(Uri.parse(url));
             },
             child: Padding(
               padding: EdgeInsets.symmetric(vertical: 8),
-              child: Text('rustdesk.com',
+              child: Text('SnowSurfer.cn',
                   style: TextStyle(
                     decoration: TextDecoration.underline,
                   )),
