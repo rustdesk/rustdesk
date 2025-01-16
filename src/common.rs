01b46164d5962327374a12979a2eb8df1372bfca
ygxz5566 <gaoxiongyu55@gmail.com>
2025-01-16 12:46:56 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://38.12.25.56:2114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
