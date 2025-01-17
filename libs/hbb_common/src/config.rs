026bfd0c4853fe98098444237377b86df371f71d
AsiaKing1103 <132553794+AsiaKing1103@users.noreply.github.com>
2025-01-17 14:41:12 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["All-FastData.myasustor.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["RustDesk-AllWorld.synology.me"];
 pub const PUBLIC_RS_PUB_KEY: &str = "7533967";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
