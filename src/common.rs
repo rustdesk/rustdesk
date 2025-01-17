4e4359c0681ec574d6da85ab69a9ade1e7a58594
AsiaKing1103 <132553794+AsiaKing1103@users.noreply.github.com>
2025-01-17 14:46:03 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://All-FastData.myasustor.com:21114".to_owned()
+    "http://RustDesk-AllWorld.synology.me:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
