97baedf530f9f22112104309c2ee2d1ddd218926
1411v6 <45678155+1411v6@users.noreply.github.com>
2025-01-23 11:53:43 +0800

    更新

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["op.nasvpn.vip"];
+pub const PUBLIC_RS_PUB_KEY: &str = "b9rdwhukQDmJ6q6ZxXx02bK2BIHUrOJq+eFjUZDcDWQ=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
