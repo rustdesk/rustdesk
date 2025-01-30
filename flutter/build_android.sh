17f0cce85743a570c0f0e75d9b02812e5cd8d0ec
hasan koç <76748696+HasanKoc33@users.noreply.github.com>
2025-01-30 04:05:17 +0300

    Update build_android.sh

--- flutter/build_android.sh
+++ flutter/build_android.sh
@@ -2,6 +2,8 @@
 
 MODE=${MODE:=release}
 $ANDROID_NDK_HOME/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-strip android/app/src/main/jniLibs/arm64-v8a/*
+# Mac OS to build android
+#$ANDROID_NDK_HOME/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/bin/aarch64-linux-android-strip android/app/src/main/jniLibs/arm64-v8a/*
 flutter build apk --target-platform android-arm64,android-arm --${MODE} --obfuscate --split-debug-info ./split-debug-info
 flutter build apk --split-per-abi --target-platform android-arm64,android-arm --${MODE} --obfuscate --split-debug-info ./split-debug-info
 flutter build appbundle --target-platform  android-arm64,android-arm --${MODE} --obfuscate --split-debug-info ./split-debug-info
