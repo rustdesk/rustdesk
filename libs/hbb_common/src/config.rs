4840421e1fee2919e0384ed0b41018e234da6289
xiao feng <65939241+syscc@users.noreply.github.com>
2025-02-14 19:44:03 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.yyej.com1"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.yyej.com"];
 pub const PUBLIC_RS_PUB_KEY: &str = "01010001";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
