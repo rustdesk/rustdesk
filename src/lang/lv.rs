9483472f820ae8c9dd772a14feb71863216e1160
Kleofass <4000163+Kleofass@users.noreply.github.com>
2025-02-22 20:17:14 +0200

    Update lv.rs

--- src/lang/lv.rs
+++ src/lang/lv.rs
@@ -656,6 +656,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Update client clipboard", "Atjaunināt klienta starpliktuvi"),
         ("Untagged", "Neatzīmēts"),
         ("new-version-of-{}-tip", "Ir pieejama jauna {} versija"),
-        ("Accessible devices", ""),
+        ("Accessible devices", "Pieejamas ierīces"),
     ].iter().cloned().collect();
 }
