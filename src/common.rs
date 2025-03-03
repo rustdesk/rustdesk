0ecaa19685a4ec1835c269cd502341ff0ae8e702
xiaochen <1521454721@qq.com>
2025-03-03 18:33:03 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -934,7 +934,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://xiaochen.cyou:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
