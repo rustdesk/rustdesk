2cf19da9305d1d4cdc32475ec986cd5bf42dfd5a
gkaigk <gkaigk@gmail.com>
2025-03-05 15:37:09 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -73,7 +73,7 @@ lazy_static::lazy_static! {
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), secrets.RS_PRV_PWD.to_string());
+        map.insert("password".to_string(), ${{ secrets.SECRET_NAME }}.to_string());
         RwLock::new(map)
     };
 
