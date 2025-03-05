d5646e2a19d923b1a94d8adf4dc25cf90546b41f
gkaigk <gkaigk@gmail.com>
2025-03-05 15:49:56 +0900

    nothing

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -71,6 +71,12 @@ lazy_static::lazy_static! {
     pub static ref DEFAULT_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
+    pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
+        let mut map = HashMap::new();
+        map.insert("password".to_string(), secrets.RS_PRV_PWD.to_string());
+        RwLock::new(map)
+    };
+
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
 }
 
