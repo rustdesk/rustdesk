ffd44f7a9032381b8a73102ef65e8fd7aa589468
mehdihaghshenas <mhaghshenas@gmail.com>
2025-03-11 19:36:34 -0700

    Fix

--- src/core_main.rs
+++ src/core_main.rs
@@ -225,7 +225,7 @@ pub fn core_main() -> Option<Vec<String>> {
                 #[cfg(windows)]
                 if crate::virtual_display_manager::is_virtual_display_supported() {
                     hbb_common::allow_err!(
-                        crate::virtual_display_manager::rustdesk_idd::install_update_driver()
+                        crate::virtual_display_manager::techdesk_idd::install_update_driver()
                     );
                 }
                 return None;
@@ -503,7 +503,7 @@ pub fn core_main() -> Option<Vec<String>> {
             }
             return None;
         } else if args[0] == "-gtk-sudo" {
-            // rustdesk service kill `rustdesk --` processes
+            // techdesk service kill `techdesk --` processes
             #[cfg(target_os = "linux")]
             if args.len() > 2 {
                 crate::platform::gtk_sudo::exec();
