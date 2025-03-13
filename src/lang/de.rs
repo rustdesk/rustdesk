2dbff45588a3b62eae8516d78ed69f80f33fb9e4
Mr-Update <37781396+Mr-Update@users.noreply.github.com>
2025-03-13 02:36:27 +0100

    Update de.rs (#11106)
    Co-authored-by: RustDesk <71636191+rustdesk@users.noreply.github.com>

--- src/lang/de.rs
+++ src/lang/de.rs
@@ -659,9 +659,9 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Accessible devices", "Erreichbare Geräte"),
         ("View camera", "Kamera anzeigen"),
         ("upgrade_remote_rustdesk_client_to_{}_tip", "Bitte aktualisieren Sie den RustDesk-Client auf der Remote-Seite auf Version {} oder neuer!"),
-        ("view_camera_unsupported_tip", ""),
-        ("Enable camera", ""),
-        ("No cameras", ""),
+        ("view_camera_unsupported_tip", "Das entfernte Gerät kann die Kamera nicht anzeigen."),
+        ("Enable camera", "Kamera zulassen"),
+        ("No cameras", "Keine Kameras"),
         ("d3d_render_tip", ""),
         ("Use D3D rendering", ""),
     ].iter().cloned().collect();
