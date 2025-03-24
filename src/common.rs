1673fc721b692afe2868184898f85d9233199e42
queque1 <553769675@qq.com>
2025-03-24 23:21:15 +0800

    修改子模块仓库地址

--- src/common.rs
+++ src/common.rs
@@ -940,7 +940,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://www.schuangtu.cn:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
