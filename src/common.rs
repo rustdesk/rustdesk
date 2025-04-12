c1157b651f9ad6c0ad2a27e4a6263b842cca883a
shenlanyw <shenlanyw@gmail.com>
2025-04-12 22:42:11 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -944,7 +944,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rust.fanoyi.com:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
