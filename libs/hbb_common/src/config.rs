5ae5d57f73a9b442e1ed9af6dfdf459c20b52b32
lovehfn <saint@mmnn.me>
2025-04-16 15:35:58 +0800

    update

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,16 +100,16 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.master.mmnn.online"];
+pub const PUBLIC_RS_PUB_KEY: &str = "MIJypXZwanshs6GI9uehOEH5BPhs502cfTqQw3gsU3w=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
     _ => PUBLIC_RS_PUB_KEY,
 };
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
+pub const RENDEZVOUS_PORT: i32 = 37210;
+pub const RELAY_PORT: i32 = 37211;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
