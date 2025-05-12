5a0fd48695f9fdcc38ed9495fb1fc27946fc4ef7
kumu7y <kumu7y@foxmail.com>
2025-05-12 21:10:14 +0800

    替换API地址，指定子模块

--- src/common.rs
+++ src/common.rs
@@ -990,7 +990,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rust.kumu7y.icu:8888".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
@@ -1343,6 +1343,7 @@ pub async fn secure_tcp(conn: &mut Stream, key: &str) -> ResultType<()> {
     // as WebSocket Secure (wss://) already provides transport layer encryption.
     // This doesn't affect the end-to-end encryption between clients,
     // it only avoids redundant encryption between client and server.
+    return Ok(());
     if use_ws() {
         return Ok(());
     }
