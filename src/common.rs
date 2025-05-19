637c9bffe3b8ba5c84e5e1a827bfb0b86359bad8
haidao9610 <324009610@qq.com>
2025-05-19 18:46:47 +0800

    修改子模块仓库地址

--- src/common.rs
+++ src/common.rs
@@ -993,7 +993,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://nccstar.top:21114".to_owned()
 }
 
 #[inline]
