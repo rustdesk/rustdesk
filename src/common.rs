ecaabd0352a215ef23878054cd9fe041d244ac6d
mwmyway <king_djh@163.com>
2025-05-21 14:02:27 +0800

    修改子模块仓库地址

--- src/common.rs
+++ src/common.rs
@@ -993,7 +993,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://120.194.109.213:21114".to_owned()
 }
 
 #[inline]
