0cb3179d5f4b2a8de43ec87e88cb5045fce8d5b8
letiandanta <249844555@qq.com>
2025-05-26 03:57:00 +0800

    Update and replace API address

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rustdesk.yunyyds.com".to_owned()
 }
 
 #[inline]
