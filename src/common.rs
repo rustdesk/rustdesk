18ea3a4b59b052e0fee0a3f200b008c90b49bd35
RustDesk <71636191+rustdesk@users.noreply.github.com>
2025-06-24 21:38:36 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1807,7 +1807,7 @@ async fn stun_ipv4_test(stun_server: &str) -> ResultType<(SocketAddr, String)> {
 static STUNS_V4: [&str; 3] = [
     "stun.l.google.com:19302",
     "stun.cloudflare.com:3478",
-    "stun.chat.bilibili.com:3478",
+    "stun.nextcloud.com:3478",
 ];
 
 static STUNS_V6: [&str; 3] = [
