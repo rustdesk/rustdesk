6ed4ee72f83196f46907aa0df75e938132d0f773
liang9411 <1477473132@qq.com>
2025-07-23 16:01:06 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://192.168.2.188:21114".to_owned()
 }
 
 #[inline]
