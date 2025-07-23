e3dc137c0706c76885207e08809a3e51ed3c878d
niuyunlong <62708109+niuyunlong@users.noreply.github.com>
2025-07-24 02:11:10 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://api-rustdesk.skpgo.com".to_owned()
 }
 
 #[inline]
