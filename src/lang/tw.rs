f34eefc7089cd656dd66b6709fe1d2b779b9d920
XLion <xlion@xlion.tw>
2025-07-25 02:11:11 +0800

    Update tw.rs

--- src/lang/tw.rs
+++ src/lang/tw.rs
@@ -709,6 +709,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("The user is not an administrator.", "使用者並不是系統管理員"),
         ("Failed to check if the user is an administrator.", "檢查使用者是否是系統管理員時失敗了"),
         ("Supported only in the installed version.", "僅支援於已安裝的版本"),
-        ("elevation_username_tip", ""),
+        ("elevation_username_tip", "輸入使用者名稱或網域\\使用者名稱"),
     ].iter().cloned().collect();
 }
