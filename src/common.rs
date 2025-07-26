b8a61e7a2639d8b4517b1da0be5989a583e6a89b
Tjccz <tjccz@qq.com>
2025-07-26 16:42:30 +0800

    更新子模块 hbb_common 到修改后的版本

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rustdesk.xwlemd.com:21114".to_owned()
 }
 
 #[inline]
