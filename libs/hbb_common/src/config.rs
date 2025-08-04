87e102ddf679d32879a57605258b362c0ea71345
Edcarlos Melo <130789814+kabaweb@users.noreply.github.com>
2025-08-04 16:14:17 -0300

    logobmdesk

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["desk.bmhelp.click"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["bmdesk.bmhelp.click"];
 pub const RS_PUB_KEY: &str = "SvejjC0uNtWqYszOegzndJbrgb8WVKrFlHuiU70oHI4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
