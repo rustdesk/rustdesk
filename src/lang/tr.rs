5dde7f5a4c1a6a89b34fa74c8d1be989b1eb6b51
SALİH ÖZKARA <salihozkara00@gmail.com>
2025-08-05 20:41:57 +0300

    Fix Turkish localization

--- src/lang/tr.rs
+++ src/lang/tr.rs
@@ -135,7 +135,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Refresh", "Yenile"),
         ("ID does not exist", "ID bulunamadı"),
         ("Failed to connect to rendezvous server", "ID oluşturma sunucusuna bağlanılamadı"),
-        ("Please try later", "Dağa sonra tekrar deneyiniz"),
+        ("Please try later", "Daha sonra tekrar deneyiniz"),
         ("Remote desktop is offline", "Uzak masaüstü kapalı"),
         ("Key mismatch", "Anahtar uyumlu değil"),
         ("Timeout", "Zaman aşımı"),
