f14b671f88933484334389a9398af5e2e6cf9b99
周子琪 <4948472@qq.com>
2025-08-15 08:40:36 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://desk.vuz.me:21114".to_owned()
 }
 
 #[inline]
