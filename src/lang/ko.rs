a4dac722d3fb7297ae124a2d1a915a8d49986921
VenusGirl❤ <venusgirl@outlook.com>
2025-08-28 16:33:12 +0900

    Update ko.rs
    Update Korean

--- src/lang/ko.rs
+++ src/lang/ko.rs
@@ -709,6 +709,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Supported only in the installed version.", "설치된 버전에서만 지원됩니다."),
         ("elevation_username_tip", "사용자 이름 또는 도메인\\사용자 이름 입력"),
         ("Preparing for installation ...", "설치 준비 중 ..."),
-        ("Show my cursor", ""),
+        ("Show my cursor", "내 커서 표시"),
     ].iter().cloned().collect();
 }
