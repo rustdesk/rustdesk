438cef8cf98c312a6c23d912877be79e9bf1d161
bovirus <1262554+bovirus@users.noreply.github.com>
2025-08-30 06:08:56 +0200

    Italian language update (#12762)

--- src/lang/it.rs
+++ src/lang/it.rs
@@ -709,6 +709,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Supported only in the installed version.", "Supportato solo nella versione installata."),
         ("elevation_username_tip", "Inserisci Nome utente o dominio sorgente\\nome Utente"),
         ("Preparing for installation ...", "Preparazione per l'installazione..."),
-        ("Show my cursor", ""),
+        ("Show my cursor", "Visualizza il mio cursore"),
     ].iter().cloned().collect();
 }
