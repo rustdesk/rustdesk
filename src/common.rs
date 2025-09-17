75792e7c13fabb64170668a0127857e76398544e
敷衍 <fuyan@fuyandeMac-mini.local>
2025-09-18 05:58:20 +0800

    修改替换 API 地址

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://nice.osapp.vip:21114".to_owned()
 }
 
 #[inline]
