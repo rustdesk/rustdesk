a1f2064c8df343ba859c673cd4b9e00c7fe9b3ab
Jonathan Gilbert <logic@deltaq.org>
2025-09-18 09:00:57 -0500

    Fixed references to min to use std::min.

--- flutter/windows/runner/main.cpp
+++ flutter/windows/runner/main.cpp
@@ -139,7 +139,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   Win32Window::Point relative_origin(10, 10);
 
   Win32Window::Point origin(workarea_origin.x + relative_origin.x, workarea_origin.y + relative_origin.y);
-  Win32Window::Size size(min(800u, workarea_size.width - relative_origin.x), min(600u, workarea_size.height - relative_origin.y));
+  Win32Window::Size size(std::min(800u, workarea_size.width - relative_origin.x), std::min(600u, workarea_size.height - relative_origin.y));
 
   std::wstring window_title;
   if (is_cm_page) {
