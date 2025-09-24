e3f60256a4776d69e34d704c52a8f4ab705d9a10
Llh0227 <102984335+Llh0227@users.noreply.github.com>
2025-09-24 22:54:50 +0800

    Update MainService.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
@@ -46,7 +46,7 @@ import java.nio.ByteBuffer
 import kotlin.math.max
 import kotlin.math.min
 
-const val DEFAULT_NOTIFY_TITLE = "RustDesk"
+const val DEFAULT_NOTIFY_TITLE = "佳影远程协助"
 const val DEFAULT_NOTIFY_TEXT = "Service is running"
 const val DEFAULT_NOTIFY_ID = 1
 const val NOTIFY_ID_OFFSET = 100
