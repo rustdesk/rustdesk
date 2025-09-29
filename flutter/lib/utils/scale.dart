3c27da4e228b50b1f9c43abf5dd337c7abb6a96a
Alessandro De Blasis <alex@deblasis.net>
2025-09-29 10:41:01 +0200

    refactor(scale): Remove unused import for web bridge
    - Eliminated the conditional import of the web bridge from scale.dart, as it is no longer necessary. This change helps to clean up the code and improve maintainability by removing unused dependencies.
    
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

--- flutter/lib/utils/scale.dart
+++ flutter/lib/utils/scale.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_hbb/consts.dart';
 import 'package:flutter_hbb/models/platform_model.dart';
-
-import 'package:flutter_hbb/generated_bridge.dart'
-    if (dart.library.html) 'package:flutter_hbb/web/bridge.dart';
 import 'package:uuid/uuid.dart';
 
 /// Clamp custom scale percent to supported bounds.
