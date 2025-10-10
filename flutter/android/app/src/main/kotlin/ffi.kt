23626ab27484cfc1600bb143eb7da1e0a28fc3ea
Llh0227 <102984335+Llh0227@users.noreply.github.com>
2025-10-11 00:32:38 +0800

    Update ffi.kt

--- flutter/android/app/src/main/kotlin/ffi.kt
+++ flutter/android/app/src/main/kotlin/ffi.kt
@@ -5,7 +5,7 @@ package ffi
 import android.content.Context
 import java.nio.ByteBuffer
 
-import com.carriez.flutter_hbb.RdClipboardManager
+import com.jyycxz.joy.RdClipboardManager
 
 object FFI {
     init {
