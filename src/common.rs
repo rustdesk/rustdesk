eea27216db85e63e9288c7762d17e089ff8621bd
EDJianXian <dannyqiu08@163.com>
2025-10-16 15:29:02 +0800

    Change API address

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rd.0a0b.cn".to_owned()
 }
 
 #[inline]
