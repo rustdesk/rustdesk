95d55fc957436926414f10c447c82fa7adabd274
yunnl <yunnl@qq.com>
2025-10-18 14:42:32 +0800

    改为自己的ID/中继服务器

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://106.52.19.173:21114".to_owned()
 }
 
 #[inline]
