213880c14db4514a26bc4cc4e04628f344327f11
bovirus <1262554+bovirus@users.noreply.github.com>
2025-11-01 07:40:17 +0100

    Italian language update (#13358)

--- src/lang/it.rs
+++ src/lang/it.rs
@@ -721,6 +721,6 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Show virtual joystick", "Visualizza joystick virtuale"),
         ("Edit note", "Modifica nota"),
         ("Alias", "Alias"),
-        ("ScrollEdge", ""),
+        ("ScrollEdge", "Bordo scorrimento"),
     ].iter().cloned().collect();
 }
