09d991d3986b2552fd801f1af7c5e1b667932f48
ffrrbb <franecomputer@gmail.com>
2025-11-05 09:40:37 +0100

    Add files via upload

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -5,7 +5,6 @@ import 'dart:io';
 import 'package:bot_toast/bot_toast.dart';
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:flutter/material.dart';
-import 'startup/custom_server_bootstrap.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hbb/common/widgets/overlay.dart';
 import 'package:flutter_hbb/desktop/pages/desktop_tab_page.dart';
@@ -146,7 +145,6 @@ void runMainApp(bool startService) async {
   }
   await Future.wait([gFFI.abModel.loadCache(), gFFI.groupModel.loadCache()]);
   gFFI.userModel.refreshCurrentUser();
-  await configureCustomServer();
   runApp(App());
 
   bool? alwaysOnTop;
