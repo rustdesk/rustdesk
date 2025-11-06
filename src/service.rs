ac4d1832cb4eea0e993e2edbb28c69878b2c5707
Jonathan Gilbert <logic@deltaq.org>
2025-11-06 12:50:18 -0600

    Updated the macos main() function in service.rs to locally import librustdesk::*.

--- src/service.rs
+++ src/service.rs
@@ -3,6 +3,7 @@ fn main() {}
 
 #[cfg(target_os = "macos")]
 fn main() {
+    use librustdesk::*;
     crate::common::load_custom_client();
     hbb_common::init_log(false, "service");
     crate::start_os_service();
