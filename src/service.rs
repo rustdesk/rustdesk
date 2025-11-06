e2a11237479ca2bb1b669e7537a8c2fd5084f10d
Jonathan Gilbert <logic@deltaq.org>
2025-11-06 13:08:35 -0600

    Reworked the macos main method to import librustdesk itself and then qualify references with it.

--- src/service.rs
+++ src/service.rs
@@ -3,8 +3,8 @@ fn main() {}
 
 #[cfg(target_os = "macos")]
 fn main() {
-    use librustdesk::*;
-    crate::common::load_custom_client();
+    use librustdesk;
+    librustdesk::common::load_custom_client();
     hbb_common::init_log(false, "service");
-    crate::start_os_service();
+    librustdesk::start_os_service();
 }
