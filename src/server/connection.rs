a8a43730cdfe9062c3e2ebc320786ed8372807a7
Jonathan Gilbert <logic@deltaq.org>
2025-11-06 15:16:34 -0600

    Removed the explicit import of crate::unix_file_clip, which Copilot suggested but was wrong anyway, with increasing the scope of the import of crate::clipboard_file::* to include feature "unix-file-copy-paste".

--- src/server/connection.rs
+++ src/server/connection.rs
@@ -3,10 +3,8 @@ use super::{input_service::*, *};
 use crate::clipboard::try_empty_clipboard_files;
 #[cfg(not(any(target_os = "android", target_os = "ios")))]
 use crate::clipboard::{update_clipboard, ClipboardSide};
-#[cfg(any(target_os = "windows", target_os = "macos"))]
+#[cfg(any(target_os = "windows", target_os = "macos", feature = "unix-file-copy-paste"))]
 use crate::clipboard_file::*;
-#[cfg(feature = "unix-file-copy-paste")]
-use crate::unix_file_clip;
 #[cfg(target_os = "android")]
 use crate::keyboard::client::map_key_to_control_key;
 #[cfg(target_os = "linux")]
