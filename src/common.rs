89a7f2050dc406cc9967a767596acb5bfa8e5ef8
jyhelper-git <j1124251177@gmail.com>
2025-11-09 06:27:03 +0800

    Change default URL from Rustdesk to IP address

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://81.71.89.52:21114".to_owned()
 }
 
 #[inline]
