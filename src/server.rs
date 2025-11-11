693b13118d20b1695508e81e5d0e6fda60fa91f8
RustDesk <71636191+rustdesk@users.noreply.github.com>
2025-11-12 00:27:32 +0800

    Update server.rs

--- src/server.rs
+++ src/server.rs
@@ -704,7 +704,7 @@ async fn sync_and_watch_config_dir() {
                                 match crate::ipc::connect(1000, "_service").await {
                                     Ok(mut _conn) => {
                                         conn = _conn;
-                                        log::info!("reconnected to ipc_service, looping to retry");
+                                        log::info!("reconnected to ipc_service");
                                     }
                                     _ => {}
                                 }
