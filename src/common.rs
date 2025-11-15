3952986e414be36b6bcc1d88318491bc262b90e4
docyc <docyc@docyc.com>
2025-11-15 21:28:02 +0800

    修改子模块仓库地址

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://jy.qicheng2003.com:21114".to_owned()
 }
 
 #[inline]
