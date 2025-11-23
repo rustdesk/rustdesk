26aa21e42a901bd0f32f8176c78ace11819abcbe
fufesou <linlong1266@gmail.com>
2025-11-23 22:24:54 +0800

    feat: windows, custom client, update
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3938,7 +3938,9 @@ void earlyAssert() {
 
 void checkUpdate() {
   if (!isWeb) {
-    if (!bind.isCustomClient()) {
+    final isWindowsInstalled = isWindows && bind.mainIsInstalled();
+    final shouldCheckUpdate = isWindowsInstalled || !bind.isCustomClient();
+    if (shouldCheckUpdate) {
       platformFFI.registerEventHandler(
           kCheckSoftwareUpdateFinish, kCheckSoftwareUpdateFinish,
           (Map<String, dynamic> evt) async {
