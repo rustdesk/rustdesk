4ed8696d1d1731f24b00c424e632a283b3b75298
fufesou <13586388+fufesou@users.noreply.github.com>
2025-11-26 19:15:32 +0800

    fix: file transfer, jobs lost if conn is not established (#13635)
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- src/ui_session_interface.rs
+++ src/ui_session_interface.rs
@@ -2009,7 +2009,7 @@ pub async fn io_loop<T: InvokeUiSession>(handler: Session<T>, round: u32) {
     }
     let mut remote = Remote::new(handler, receiver, sender);
     remote.io_loop(&key, &token, round).await;
-    remote.sync_jobs_status_to_local().await;
+    let _ = remote.sync_jobs_status_to_local().await;
 }
 
 #[cfg(not(any(target_os = "android", target_os = "ios")))]
