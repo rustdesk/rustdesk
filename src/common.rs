76b4e0aca29936d2d4781f14b7e5613dfd8e7107
masnszn <hengmingzhao@163.com>
2025-12-17 10:21:18 +0800

    修改子模块仓库地址

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://nszn.com".to_owned()
 }
 
 #[inline]
