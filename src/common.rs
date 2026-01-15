ef7469f4de5e8ff3012c5a48c11db1a006e5f53a
LJYXG <60862509+LJYXG@users.noreply.github.com>
2026-01-16 00:07:12 +0800

    change api server

--- src/common.rs
+++ src/common.rs
@@ -1085,7 +1085,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rscadmin.xggx.de".to_owned()
 }
 
 #[inline]
