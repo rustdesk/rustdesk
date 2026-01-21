d4f8f2bbac0713d4cdea158cfd3fc1a93df9309c
libin <libin.chat@outlook.com>
2026-01-21 22:30:33 +0800

    fix: missing macos.rs

---
+++ src/privacy_mode/macos.rs
@@ -0,0 +1,70 @@
+use super::{PrivacyMode, PrivacyModeState};
+use hbb_common::ResultType;
+
+extern "C" {
+    fn MacSetPrivacyMode(on: bool) -> bool;
+}
+
+pub const PRIVACY_MODE_IMPL: &str = "privacy_mode_impl_macos";
+
+pub struct PrivacyModeImpl {
+    impl_key: String,
+    conn_id: i32,
+}
+
+impl PrivacyModeImpl {
+    pub fn new(impl_key: &str) -> Self {
+        Self {
+            impl_key: impl_key.to_owned(),
+            conn_id: 0,
+        }
+    }
+}
+
+impl PrivacyMode for PrivacyModeImpl {
+    fn is_async_privacy_mode(&self) -> bool {
+        false
+    }
+
+    fn init(&self) -> ResultType<()> {
+        Ok(())
+    }
+
+    fn clear(&mut self) {
+        unsafe {
+            MacSetPrivacyMode(false);
+        }
+    }
+
+    fn turn_on_privacy(&mut self, conn_id: i32) -> ResultType<bool> {
+        self.conn_id = conn_id;
+        unsafe {
+            MacSetPrivacyMode(true);
+        }
+        Ok(true)
+    }
+
+    fn turn_off_privacy(&mut self, _conn_id: i32, _state: Option<PrivacyModeState>) -> ResultType<()> {
+        unsafe {
+            MacSetPrivacyMode(false);
+        }
+        self.conn_id = 0;
+        Ok(())
+    }
+
+    fn pre_conn_id(&self) -> i32 {
+        self.conn_id
+    }
+
+    fn get_impl_key(&self) -> &str {
+        &self.impl_key
+    }
+}
+
+impl Drop for PrivacyModeImpl {
+    fn drop(&mut self) {
+        unsafe {
+            MacSetPrivacyMode(false);
+        }
+    }
+}
