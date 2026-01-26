1f35830570372c89cb33128599f28ae5403a5d96
hatterp <cyberitsec@proton.me>
2026-01-26 07:11:41 +0100

    Update pl.rs (#14112)
    updated PL translation

--- src/lang/pl.rs
+++ src/lang/pl.rs
@@ -737,7 +737,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("rel-mouse-exit-{}-tip", "Aby wyłączyć tryb przechwytywania myszy, naciśnij {}"),
         ("rel-mouse-permission-lost-tip", "Utracono uprawnienia do trybu przechwytywania myszy"),
         ("Changelog", "Dziennik zmian"),
-        ("keep-awake-during-outgoing-sessions-label", ""),
-        ("keep-awake-during-incoming-sessions-label", ""),
+        ("keep-awake-during-outgoing-sessions-label", "Utrzymuj urządzenie w stanie aktywnym podczas sesji wychodzących"),
+        ("keep-awake-during-incoming-sessions-label", "Utrzymuj urządzenie w stanie aktywnym podczas sesji przychodzących"),
     ].iter().cloned().collect();
 }
