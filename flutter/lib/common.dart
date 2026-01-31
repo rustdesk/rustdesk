b20d099bf77a273b108726f190fa2305e50c2cea
Bia189 <yinwenche@Outlook.com>
2026-01-31 19:29:56 +0800

    添加内嵌字体

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -402,6 +402,7 @@ class MyTheme {
             ),
           )
         : null,
+    fontFamily: 'SourceHanSansSC',
     textTheme: const TextTheme(
         titleLarge: TextStyle(fontSize: 19, color: Colors.black87),
         titleSmall: TextStyle(fontSize: 14, color: Colors.black87),
@@ -500,6 +501,7 @@ class MyTheme {
             ),
           )
         : null,
+    fontFamily: 'SourceHanSansSC',
     textTheme: const TextTheme(
       titleLarge: TextStyle(fontSize: 19),
       titleSmall: TextStyle(fontSize: 14),
@@ -1579,7 +1581,7 @@ bool option2bool(String option, String value) {
       option == kOptionForceAlwaysRelay) {
     res = value == "Y";
   } else {
-    // "" is true
+    assert(false);
     res = value != "N";
   }
   return res;
@@ -1597,6 +1599,9 @@ String bool2option(String option, bool b) {
       option == kOptionForceAlwaysRelay) {
     res = b ? 'Y' : defaultOptionNo;
   } else {
+    if (option != kOptionEnableUdpPunch && option != kOptionEnableIpv6Punch) {
+      assert(false);
+    }
     res = b ? 'Y' : 'N';
   }
   return res;
@@ -2682,43 +2687,19 @@ class SimpleWrapper<T> {
 /// This manager handles multiple tabs within the same isolate.
 class WakelockManager {
   static final Set<UniqueKey> _enabledKeys = {};
-  // Don't use WakelockPlus.enabled, it causes error on Android:
-  // Unhandled Exception: FormatException: Message corrupted
-  //
-  // On Linux, multiple enable() calls create only one inhibit, but each disable()
-  // only releases if _cookie != null. So we need our own _enabled state to avoid
-  // calling disable() when not enabled.
-  // See: https://github.com/fluttercommunity/wakelock_plus/blob/0c74e5bbc6aefac57b6c96bb7ef987705ed559ec/wakelock_plus/lib/src/wakelock_plus_linux_plugin.dart#L48
-  static bool _enabled = false;
 
-  static void enable(UniqueKey key, {bool isServer = false}) {
-    // Check if we should keep awake during outgoing sessions
-    if (!isServer) {
-      final keepAwake =
-          mainGetLocalBoolOptionSync(kOptionKeepAwakeDuringOutgoingSessions);
-      if (!keepAwake) {
-        return; // Don't enable wakelock if user disabled keep awake
-      }
-    }
-    if (isDesktop) {
-      _enabledKeys.add(key);
-    }
-    if (!_enabled) {
-      _enabled = true;
-      WakelockPlus.enable();
-    }
+  static void enable(UniqueKey key) {
+    if (isLinux) return;
+    _enabledKeys.add(key);
+    WakelockPlus.enable();
   }
 
   static void disable(UniqueKey key) {
-    if (isDesktop) {
-      _enabledKeys.remove(key);
-      if (_enabledKeys.isNotEmpty) {
-        return;
+    if (isLinux) return;
+    if (_enabledKeys.remove(key)) {
+      if (_enabledKeys.isEmpty) {
+        WakelockPlus.disable();
       }
-    }
-    if (_enabled) {
-      WakelockPlus.disable();
-      _enabled = false;
     }
   }
 }
