779b7aaf0265b0fe22e1d71b364c405db3d2231e
fufesou <13586388+fufesou@users.noreply.github.com>
2026-02-15 16:43:21 +0800

    feat(wayland): keyboard mode, legacy translate (#14317)
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- flutter/lib/desktop/widgets/remote_toolbar.dart
+++ flutter/lib/desktop/widgets/remote_toolbar.dart
@@ -1861,8 +1861,18 @@ class _KeyboardMenu extends StatelessWidget {
           continue;
         }
 
-        if (pi.isWayland && mode.key != kKeyMapMode) {
-          continue;
+        if (pi.isWayland) {
+          // Legacy mode is hidden on desktop control side because dead keys
+          // don't work properly on Wayland. When the control side is mobile,
+          // Legacy mode is used automatically (mobile always sends Legacy events).
+          if (mode.key == kKeyLegacyMode) {
+            continue;
+          }
+          // Translate mode requires server >= 1.4.6.
+          if (mode.key == kKeyTranslateMode &&
+              versionCmp(pi.version, '1.4.6') < 0) {
+            continue;
+          }
         }
 
         var text = translate(mode.menu);
