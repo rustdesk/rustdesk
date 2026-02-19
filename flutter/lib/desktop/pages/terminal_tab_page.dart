441f6a35d0eb6b0d97f7d46f7ddb034ee7bb3f43
fufesou <linlong1266@gmail.com>
2026-02-19 10:17:57 +0800

    fix(terminal): fix new tab auto-focus and NaN error on data before layout
    - Fix new tab not auto-focusing: add FocusNode to TerminalView and
      request focus when tab is selected via tab state listener
    - Fix NaN error when data arrives before terminal view layout: buffer
      output data until terminal view has valid dimensions, flush on first
      valid resize callback
    
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- flutter/lib/desktop/pages/terminal_tab_page.dart
+++ flutter/lib/desktop/pages/terminal_tab_page.dart
@@ -92,6 +92,7 @@ class _TerminalTabPageState extends State<TerminalTabPage> {
         key: ValueKey(tabKey),
         id: peerId,
         terminalId: terminalId,
+        tabKey: tabKey,
         password: password,
         isSharedPassword: isSharedPassword,
         tabController: tabController,
