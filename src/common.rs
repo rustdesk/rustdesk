45fd232bd4e961083a1a430cd7a8fd11fe24dae8
infinith <72787216+MortalM@users.noreply.github.com>
2026-03-26 15:19:52 +0800

    change api

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://221.130.17.50:21114".to_owned()
 }
 
 #[inline]
