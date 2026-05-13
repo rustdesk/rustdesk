cff3c7b8f4da772ee3fa2baf6011a25a616ef577
Masruhan <masruhan.khotibb@gmail.com>
2026-05-13 12:21:03 +0700

    Update URL in GestureDetector onTap function

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3697,7 +3697,7 @@ Widget loadPowered(BuildContext context) {
     cursor: SystemMouseCursors.click,
     child: GestureDetector(
       onTap: () {
-        launchUrl(Uri.parse('https://rustdesk.com'));
+        launchUrl(Uri.parse('https://indoteknisi.com'));
       },
       child: Opacity(
           opacity: 0.5,
