9d6809a821b6ba73ca6f24b662c9c2ac2fa2fb58
Enoydu <68388226+Enoydu@users.noreply.github.com>
2026-05-24 20:46:53 +0800

    Update desktop_home_page.dart

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -201,7 +201,7 @@ Widget _buildContactInfo(BuildContext context) {
     return ChangeNotifierProvider.value(
       value: gFFI.serverModel,
       child: Container(
-        width: isIncomingOnly ? 280.0 : 200.0,
+        width: isIncomingOnly ? 280.0 : 260.0,
         color: Theme.of(context).colorScheme.background,
         child: Stack(
           children: [
