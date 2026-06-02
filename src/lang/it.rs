ee6ccf7ea692c853c6379e78c49333b5326f4876
bovirus <1262554+bovirus@users.noreply.github.com>
2026-06-02 12:18:30 +0200

    Update it.rs

--- src/lang/it.rs
+++ src/lang/it.rs
@@ -751,12 +751,12 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("server_requires_deployment_tip", "Il server richiede che questo dispositivo venga distribuito in modo esplicito.\nVuoi distribuirlo?"),
         ("The server does not require explicit deployment.", "Il server non richiede una distribuzione esplicita."),
         ("Unknown response.", "Risposta sconosciuta"),
-        ("wayland-keyboard-input-disabled-tip", ""),
-        ("wayland-keyboard-input-consent-tip", ""),
-        ("wayland-keyboard-input-applies-to-tip", ""),
-        ("wayland-soft-keyboard-input-label", ""),
-        ("wayland-keyboard-input-reset-choice-tip", ""),
-        ("remember-wayland-keyboard-choice-tip", ""),
+        ("wayland-keyboard-input-disabled-tip", "Vuoi consentire l'input da tastiera?"),
+        ("wayland-keyboard-input-consent-tip", "Ciò che digiti in questo computer remoto (comprese le password) potrebbe essere letto da altre app prenti nel computer remoto."),
+        ("wayland-keyboard-input-applies-to-tip", "Questa scelta si applica a:"),
+        ("wayland-soft-keyboard-input-label", "Inserimento tramite tastiera soft"),
+        ("wayland-keyboard-input-reset-choice-tip", "Ripristina scelta input da tastiera"),
+        ("remember-wayland-keyboard-choice-tip", "Non chiedere più per questo computer remoto"),
         ("Why this happens", ""),
     ].iter().cloned().collect();
 }
