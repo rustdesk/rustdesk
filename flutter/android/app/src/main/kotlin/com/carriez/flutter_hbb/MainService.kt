582d4f35e02ac47907a2ee3b1fd58615d6efad19
imovely66-afk <imovely66@gmail.com>
2026-06-05 16:38:24 +0000

    新修改的

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
@@ -346,7 +346,21 @@ class MainService : Service() {
                 requestMediaProjection()
             }
         }
-        return START_NOT_STICKY // don't use sticky (auto restart), the new service (from auto restart) will lose control
+        return START_STICKY
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+        Log.d(logTag, "onTaskRemoved, restarting service")
+        val restartIntent = Intent(this, MainService::class.java).apply {
+            action = ACT_INIT_MEDIA_PROJECTION_AND_SERVICE
+            putExtra(EXT_INIT_FROM_BOOT, true)
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(restartIntent)
+        } else {
+            startService(restartIntent)
+        }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
