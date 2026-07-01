c858ccfd806acc687a826d307de9b3a83b85e050
longwanglaomu <107171137+longwanglaomu@users.noreply.github.com>
2026-07-01 10:23:40 +0800

    Update rendezvous_mediator.rs
    去掉更新

--- src/rendezvous_mediator.rs
+++ src/rendezvous_mediator.rs
@@ -121,10 +121,10 @@ impl RendezvousMediator {
             }
         }
         crate::hbbs_http::sync::start();
-        #[cfg(target_os = "windows")]
-        if crate::platform::is_installed() && crate::is_server() {
-            crate::updater::start_auto_update();
-        }
+        //#[cfg(target_os = "windows")]
+        //if crate::platform::is_installed() && crate::is_server() {
+        //    crate::updater::start_auto_update();
+        //}
         check_zombie();
         let server = new_server();
         if config::option2bool("stop-service", &Config::get_option("stop-service")) {
