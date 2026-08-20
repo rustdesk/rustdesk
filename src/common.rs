798b73beb120d333902e4ff87a5e7587f343320c
RustDesk <71636191+rustdesk@users.noreply.github.com>
2026-08-21 00:52:31 +0800

    Update common.rs (#15924)

--- src/common.rs
+++ src/common.rs
@@ -1088,7 +1088,7 @@ fn get_api_server_(api: String, custom: String) -> String {
 #[inline]
 pub fn is_public(url: &str) -> bool {
     let url = url.to_ascii_lowercase();
-    url.contains("rustdesk.com/") || url.ends_with("rustdesk.com")
+    url.contains(".rustdesk.com")
 }
 
 pub fn get_udp_punch_enabled() -> bool {
