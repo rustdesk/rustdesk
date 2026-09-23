d4219db2e894acbf5767d46b0a407816fea02651
21pages <sunboeasy@gmail.com>
2026-09-23 09:57:43 +0800

    fix: restrict VRAM stall detection to AMD SDK

--- libs/scrap/src/common/vram.rs
+++ libs/scrap/src/common/vram.rs
@@ -118,12 +118,15 @@ impl EncoderApi for VRamEncoder {
             });
         }
         if frames.len() > 0 {
-            // This kind of problem is occurred after a period of time when using AMD encoding,
+            // This kind of problem is occurred after a period of time when using the AMD SDK,
             // the encoding length is fixed at about 40, and the picture is still
             const MIN_BAD_LEN: usize = 100;
             const MAX_BAD_COUNTER: usize = 30;
             let this_frame_len = frames[0].data.len();
-            if this_frame_len < MIN_BAD_LEN && this_frame_len == self.last_frame_len {
+            if self.ctx.f.driver == Driver::AMF
+                && this_frame_len < MIN_BAD_LEN
+                && this_frame_len == self.last_frame_len
+            {
                 self.same_bad_len_counter += 1;
                 if self.same_bad_len_counter >= MAX_BAD_COUNTER {
                     log::info!(
